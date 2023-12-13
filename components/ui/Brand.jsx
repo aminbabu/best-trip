a0635cb7a47419e494f7032efacbe8d43f4235d3
Amin Babu <amin.babu.bd@gmail.com>
2023-12-14 00:42:16 +0600

    Refactor Navbar component to include Brand logo

---
+++ components/ui/Brand.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Brand = ({ href = "/", logo, width, height, alt, ...props }) => {
+  return (
+    <Link href={href} {...props}>
+      {src ? (
+        <Image src={logo} width={width} height={height} alt={alt} />
+      ) : (
+        alt || "Brand"
+      )}
+    </Link>
+  );
+};
+
+export default Brand;
