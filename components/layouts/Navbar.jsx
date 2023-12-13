5c9b72cfaaf14c9ac8649c65f9bd04a54e4ccfd7
Amin Babu <amin.babu.bd@gmail.com>
2023-12-14 00:45:01 +0600

    Refactor Navbar component to include Brand and Container components

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,5 +1,20 @@
+import Brand from "../ui/Brand";
+import Container from "./Container";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav>
+      <Container className="flex items-center gap-4 justify-between">
+        <Brand
+          logo="/images/brand-logo.png"
+          width="108"
+          height="46"
+          alt="Best Trip"
+        />
+        
+      </Container>
+    </nav>
+  );
 };
 
 export default Navbar;
