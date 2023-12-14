b6bb36e4f2113d15dbb6cd9c35f81de6f9a83e95
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 01:04:20 +0600

    Refactor Newsletter layout

--- components/layouts/Newsletter.jsx
+++ components/layouts/Newsletter.jsx
@@ -4,17 +4,15 @@ import NewsletterForm from "./NewsletterForm";
 const Newsletter = () => {
   return (
     <section className="py-12 sm:py-16 md:py-20 lg:py-24 xl:pt-[120px] xl:pb-[100px]">
-      <Container>
-        <h2 className="text-3xl lg:text-4xl text-t-800 font-semibold text-center mb-6 lg:mb-8">
-          Subscribe to Get Special Price
-        </h2>
-        <p className="text-t-600 text-center mb-12">
-          Do not wanna miss something? Subscribe right now and get special{" "}
-          <br className="hidden sm:block" />
-          promotion and monthly newalwtter
-        </p>
-        <NewsletterForm />
-      </Container>
+      <h2 className="text-3xl lg:text-4xl text-t-800 font-semibold text-center mb-6 lg:mb-8">
+        Subscribe to Get Special Price
+      </h2>
+      <p className="text-t-600 text-center mb-12">
+        Do not wanna miss something? Subscribe right now and get special{" "}
+        <br className="hidden sm:block" />
+        promotion and monthly newalwtter
+      </p>
+      <NewsletterForm />
     </section>
   );
 };
