5018d0754c85cc9fecf74b4b1e9d21e15433616b
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 01:02:42 +0600

    Add newsletter form with validation and submission

--- components/layouts/NewsletterForm.jsx
+++ components/layouts/NewsletterForm.jsx
@@ -1,5 +1,64 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+});
+
 const NewsletterForm = () => {
-  return <div>NewsletterForm</div>;
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="relative max-w-2xl mx-auto"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl className="h-16 pr-36 lg:pr-48">
+                <Input placeholder="Enter your mail address" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="absolute top-1.5 right-2 px-6 lg:px-11"
+        >
+          Subscribe
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default NewsletterForm;
