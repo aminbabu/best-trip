44999a768facb354f6b2cf91e5d5eb4b7b6544a8
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 01:11:51 +0600

    Refactor Footer component layout

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -1,9 +1,16 @@
+import Container from "./Container";
+import Copyright from "./Copyright";
 import Newsletter from "./Newsletter";
 
 const Footer = () => {
   return (
     <footer className="bg-secondary mt-auto">
-      <Newsletter />
+      <Container className="grid gap-10">
+        <Newsletter />
+        <hr className="border-[#d7d7d7]/50" />
+        <hr className="border-[#d7d7d7]/50" />
+        <Copyright />
+      </Container>
     </footer>
   );
 };
