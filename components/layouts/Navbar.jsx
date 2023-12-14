3e91426bdc1655cc91d8a7f7fcf7c145449037cd
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 00:02:32 +0600

    Add menu, navbar CTA, and nav sheet components to Navbar.jsx

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,17 +1,26 @@
 import Brand from "../ui/Brand";
 import Container from "./Container";
+import Menu from "./Menu";
+import NavbarCta from "./NavbarCta";
+import NavSheet from "./NavSheet";
 
 const Navbar = () => {
   return (
     <nav>
-      <Container className="flex items-center gap-4 justify-between">
+      <Container className="flex items-center gap-4 justify-between py-6">
         <Brand
           logo="/images/brand-logo.png"
           width="108"
           height="46"
           alt="Best Trip"
         />
-        
+        <div className="hidden lg:block">
+          <Menu />
+        </div>
+        <div className="hidden lg:block">
+          <NavbarCta />
+        </div>
+        <NavSheet />
       </Container>
     </nav>
   );
