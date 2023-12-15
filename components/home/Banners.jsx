937a57da7962ef87b6c761b74b8b3075d410b213
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 23:48:39 +0600

    Refactor Banners component to use a Container layout

--- components/home/Banners.jsx
+++ components/home/Banners.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import SectionComponent from "../global/Section";
 import Image from "next/image";
 import Slider from "@/components/global/splide/Slider";
 import { SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import Link from "next/link";
+import Container from "../layouts/Container";
 
 const options = {
   type: "loop",
@@ -31,25 +31,27 @@ const options = {
 
 const Banners = ({ items }) => {
   return (
-    <SectionComponent>
-      <Slider hasTrack={false} options={options}>
-        <SplideTrack>
-          {items.map((item, index) => (
-            <SplideSlide key={index}>
-              <Link href="#" className="flex overflow-hidden rounded">
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  width={360}
-                  height={202}
-                  className="rounded object-cover w-full h-auto"
-                />
-              </Link>
-            </SplideSlide>
-          ))}
-        </SplideTrack>
-      </Slider>
-    </SectionComponent>
+    <section className="py-12 sm:py-16 lg:py-20 xl:py-24">
+      <Container>
+        <Slider hasTrack={false} options={options}>
+          <SplideTrack>
+            {items.map((item, index) => (
+              <SplideSlide key={index}>
+                <Link href="#" className="flex overflow-hidden rounded">
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    width={360}
+                    height={202}
+                    className="rounded object-cover w-full h-auto"
+                  />
+                </Link>
+              </SplideSlide>
+            ))}
+          </SplideTrack>
+        </Slider>
+      </Container>
+    </section>
   );
 };
 
