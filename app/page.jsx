0bf1ec6cffd520065364ef8f1e94b030d6722f2c
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 20:37:44 +0600

    Refactor Home component to dynamically render components from data

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,26 @@
+import Components from "@/components/global";
+import data from "../data/home.json";
+
 const Home = () => {
-  return <main>Home</main>;
+  return (
+    <main>
+      {data.map((item, index) => {
+        const { name, title, description, items } = item;
+        const Component = Components[name];
+
+        if (!Component) return null;
+
+        return (
+          <Component
+            key={index}
+            title={title}
+            description={description}
+            items={items}
+          />
+        );
+      })}
+    </main>
+  );
 };
 
 export default Home;
