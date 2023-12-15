d8358a01ac6dd9ef7ada8d0ad246b702b64ccbb6
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 23:52:12 +0600

    Refactor UmrahZiyarah component structure

--- components/home/UmrahZiyarah.jsx
+++ components/home/UmrahZiyarah.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { SplideSlide, SplideTrack } from "@splidejs/react-splide";
-// import Section from "@/components/global/Section";
 import SectionHeader from "@/components/global/SectionHeader";
 import Slider from "@/components/global/splide/Slider";
 import UmrahZiyarahSlideItem from "@/components/home/UmrahZiyarahSlideItem";
+import Container from "../layouts/Container";
 
 const options = {
   type: "loop",
@@ -33,20 +33,20 @@ const options = {
 
 const UmrahZiyarah = ({ title, description, items }) => {
   return (
-    <>
-      {/* <Section className="bg-secondary"> */}
-      <SectionHeader title={title} description={description} />
-      <Slider hasTrack={false} options={options}>
-        <SplideTrack>
-          {items.map((item, index) => (
-            <SplideSlide key={index}>
-              <UmrahZiyarahSlideItem item={item} />
-            </SplideSlide>
-          ))}
-        </SplideTrack>
-      </Slider>
-      {/* </Section> */}
-    </>
+    <section className="py-12 sm:py-16 lg:py-20 xl:py-24">
+      <Container>
+        <SectionHeader title={title} description={description} />
+        <Slider hasTrack={false} options={options}>
+          <SplideTrack>
+            {items.map((item, index) => (
+              <SplideSlide key={index}>
+                <UmrahZiyarahSlideItem item={item} />
+              </SplideSlide>
+            ))}
+          </SplideTrack>
+        </Slider>
+      </Container>
+    </section>
   );
 };
 
