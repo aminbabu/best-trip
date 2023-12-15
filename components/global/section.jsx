9e7b6175de9dec4fe4db63326eac8a0b1d6522ad
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 15:31:29 +0600

    Refactor section component to remove unnecessary code

--- components/global/section.jsx
+++ components/global/section.jsx
@@ -4,21 +4,7 @@ import Container from "../layouts/Container";
 const Section = ({ title, description, className, children }) => {
   return (
     <section className={cn("py-12 sm:py-16 lg:py-20 xl:py-24", className)}>
-      <Container>
-        {(title || description) && (
-          <div className="max-w-[800px] mx-auto mb-10 sm:mb-14 lg:mb-20">
-            {title && (
-              <h2 className="text-t-800 text-3xl lg:text-[2.5rem] font-semibold mb-2.5">
-                {title}
-              </h2>
-            )}
-            {description && (
-              <p className="text-t-700 text-base">{description}</p>
-            )}
-          </div>
-        )}
-        {children}
-      </Container>
+      <Container>{children}</Container>
     </section>
   );
 };
