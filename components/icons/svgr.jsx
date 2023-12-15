ae60669218faf5fd173617ad88a1ee4a702e3e7d
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 20:56:24 +0600

    Add LocationIcon and StarIcon to svgr.jsx

--- components/icons/svgr.jsx
+++ components/icons/svgr.jsx
@@ -2,5 +2,14 @@ import FacebookIcon from "@/public/icons/facebook-f.svg";
 import TwitterIcon from "@/public/icons/twitter.svg";
 import InstagramIcon from "@/public/icons/instagram.svg";
 import YoutubeIcon from "@/public/icons/youtube.svg";
+import LocationIcon from "@/public/icons/location.svg";
+import StarIcon from "@/public/icons/star.svg";
 
-export { FacebookIcon, TwitterIcon, InstagramIcon, YoutubeIcon };
+export {
+  FacebookIcon,
+  TwitterIcon,
+  InstagramIcon,
+  YoutubeIcon,
+  LocationIcon,
+  StarIcon,
+};
