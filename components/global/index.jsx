928484070d5a194a071e88ac0cdf7a94a645d704
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 21:22:27 +0600

    Update dynamic import paths for Banners and Hotels components

--- components/global/index.jsx
+++ components/global/index.jsx
@@ -1,11 +1,16 @@
 import dynamic from "next/dynamic";
 
-const Banners = dynamic(() => import("../home/banners"), {
+const Banners = dynamic(() => import("../home/Banners"), {
   loading: () => <p>Banners Loading...</p>,
+});
+
+const Hotels = dynamic(() => import("../home/Hotels"), {
+  loading: () => <p>Hotels Loading...</p>,
 });
 
 const Components = {
   banners: Banners,
+  hotels: Hotels,
 };
 
 export default Components;
