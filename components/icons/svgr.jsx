156c8991f36ebeeb434710f429917b75adea5a3b
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 08:59:49 +0600

    Add SVG icons for social media platforms

---
+++ components/icons/svgr.jsx
@@ -0,0 +1,6 @@
+import FacebookIcon from "@/public/icons/facebook-f.svg";
+import TwitterIcon from "@/public/icons/twitter.svg";
+import InstagramIcon from "@/public/icons/instagram.svg";
+import YoutubeIcon from "@/public/icons/youtube.svg";
+
+export { FacebookIcon, TwitterIcon, InstagramIcon, YoutubeIcon };
