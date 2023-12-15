25750e3ff71ca61e159062c34f780cfd96c68170
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 09:43:21 +0600

    Refactor Brand component to include className prop

--- components/ui/Brand.jsx
+++ components/ui/Brand.jsx
@@ -1,9 +1,22 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-const Brand = ({ href = "/", logo, width, height, alt, ...props }) => {
+const Brand = ({
+  href = "/",
+  logo,
+  width,
+  height,
+  alt,
+  className,
+  ...props
+}) => {
   return (
-    <Link href={href} {...props}>
+    <Link
+      href={href}
+      {...props}
+      className={cn("inline-flex items-center", className)}
+    >
       {logo ? (
         <Image src={logo} width={width} height={height} alt={alt} />
       ) : (
