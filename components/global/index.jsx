a1b3410a1e8b49022f47fca7aa58e9c439c7f345
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 21:39:37 +0600

    Add PopularDestinations component to global index.jsx

--- components/global/index.jsx
+++ components/global/index.jsx
@@ -8,9 +8,17 @@ const Hotels = dynamic(() => import("../home/Hotels"), {
   loading: () => <p>Hotels Loading...</p>,
 });
 
+const PopularDestinations = dynamic(
+  () => import("../home/PopularDestinations"),
+  {
+    loading: () => <p>Hotels Loading...</p>,
+  }
+);
+
 const Components = {
   banners: Banners,
   hotels: Hotels,
+  popular_destinations: PopularDestinations,
 };
 
 export default Components;
