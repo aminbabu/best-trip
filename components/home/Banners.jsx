2ec817788f086216f7dd6bd191a16da37bb39221
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 20:41:20 +0600

    Add Banners component for homepage

---
+++ components/home/Banners.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Section from "../global/Section";
+import Image from "next/image";
+import Slider from "../global/splide/Slider";
+import { SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import Link from "next/link";
+
+const options = {
+  type: "loop",
+  perPage: 1,
+  perMove: 1,
+  gap: 16,
+  arrows: false,
+  pagination: false,
+  autoplay: true,
+  mediaQuery: "min",
+  breakpoints: {
+    640: {
+      perPage: 2,
+    },
+    1024: {
+      perPage: 3,
+      arrows: true,
+    },
+    1280: {
+      gap: 32,
+    },
+  },
+};
+
+const Banners = ({ items }) => {
+  return (
+    <Section>
+      <Slider hasTrack={false} options={options}>
+        <SplideTrack>
+          {items.map((item, index) => (
+            <SplideSlide key={index}>
+              <Link href="#" className="flex overflow-hidden rounded">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  width={360}
+                  height={202}
+                  className="rounded object-cover w-full h-auto"
+                />
+              </Link>
+            </SplideSlide>
+          ))}
+        </SplideTrack>
+      </Slider>
+    </Section>
+  );
+};
+
+export default Banners;
