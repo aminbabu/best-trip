2a11d0dd983b055005a9d90eb00f8ea8db2aec01
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 23:51:41 +0600

    Refactor PopularDestinations component to use Container layout

--- components/home/PopularDestinations.jsx
+++ components/home/PopularDestinations.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { SplideSlide, SplideTrack } from "@splidejs/react-splide";
-// import Section from "@/components/global/Section";
 import SectionHeader from "@/components/global/SectionHeader";
 import Slider from "@/components/global/splide/Slider";
 import PopularDestinationSlideItem from "@/components/home/PopularDestinationSlideItem";
+import Container from "../layouts/Container";
 
 const options = {
   type: "loop",
@@ -33,20 +33,20 @@ const options = {
 
 const PopularDestinations = ({ title, description, items }) => {
   return (
-    <>
-      {/* <Section> */}
-      <SectionHeader title={title} description={description} />
-      <Slider hasTrack={false} options={options}>
-        <SplideTrack>
-          {items.map((item, index) => (
-            <SplideSlide key={index}>
-              <PopularDestinationSlideItem item={item} />
-            </SplideSlide>
-          ))}
-        </SplideTrack>
-      </Slider>
-      {/* </Section> */}
-    </>
+    <section className="py-12 sm:py-16 lg:py-20 xl:py-24">
+      <Container>
+        <SectionHeader title={title} description={description} />
+        <Slider hasTrack={false} options={options}>
+          <SplideTrack>
+            {items.map((item, index) => (
+              <SplideSlide key={index}>
+                <PopularDestinationSlideItem item={item} />
+              </SplideSlide>
+            ))}
+          </SplideTrack>
+        </Slider>
+      </Container>
+    </section>
   );
 };
 
