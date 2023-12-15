3810f273dc1ec3ce6c8d0a7d48acd51e325403e7
Amin Babu <amin.babu.bd@gmail.com>
2023-12-15 23:50:31 +0600

    Refactor Hotels component to use a container and add section styling

--- components/home/Hotels.jsx
+++ components/home/Hotels.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { SplideSlide, SplideTrack } from "@splidejs/react-splide";
-// import Section from "@/components/global/Section";
 import SectionHeader from "@/components/global/SectionHeader";
 import Slider from "@/components/global/splide/Slider";
 import HotelSlideItem from "@/components/home/HotelSlideItem";
+import Container from "../layouts/Container";
 
 const options = {
   type: "loop",
@@ -33,20 +33,20 @@ const options = {
 
 const Hotels = ({ title, description, items }) => {
   return (
-    <>
-      {/* <Section className="bg-secondary"> */}
-      <SectionHeader title={title} description={description} />
-      <Slider hasTrack={false} options={options}>
-        <SplideTrack>
-          {items.map((item, index) => (
-            <SplideSlide key={index}>
-              <HotelSlideItem item={item} />
-            </SplideSlide>
-          ))}
-        </SplideTrack>
-      </Slider>
-      {/* </Section> */}
-    </>
+    <section className="py-12 sm:py-16 lg:py-20 xl:py-24 bg-secondary">
+      <Container>
+        <SectionHeader title={title} description={description} />
+        <Slider hasTrack={false} options={options}>
+          <SplideTrack>
+            {items.map((item, index) => (
+              <SplideSlide key={index}>
+                <HotelSlideItem item={item} />
+              </SplideSlide>
+            ))}
+          </SplideTrack>
+        </Slider>
+      </Container>
+    </section>
   );
 };
 
