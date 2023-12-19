463c21cfaef3abb7f4458d1fbf6c5ab502e2185c
Amin Babu <amin.babu.bd@gmail.com>
2023-12-19 16:32:31 +0600

    Update tabs component structure

--- components/global/Tabs.jsx
+++ components/global/Tabs.jsx
@@ -4,19 +4,34 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
 
 const Tabs = ({ tabs, tabpanes }) => {
   return (
     <TabsComponent defaultValue={tabs[1].value}>
-      <TabsList>
-        {tabs.map((tab) => (
-          <TabsTrigger key={tab.id} value={tab.value}>
+      <TabsList className="h-auto p-0 rounded-b-none rounded-t-md">
+        {tabs.map((tab, index) => (
+          <TabsTrigger
+            key={tab.id}
+            value={tab.value}
+            className={cn(
+              "px-6 py-4 gap-x-1 text-base lg:text-lg text-t-700 bg-white rounded-none border-b border-[#D2D1D1] data-[state=active]:bg-p-900 data-[state=active]:text-white data-[state=active]:border-b-p-900",
+              {
+                "rounded-tl-md": index === 0,
+                "rounded-tr-md": index === tabs.length - 1,
+              }
+            )}
+          >
             {tab.icon} {tab.title}
           </TabsTrigger>
         ))}
       </TabsList>
       {tabpanes.map((tabpane) => (
-        <TabsContent key={tabpane.id} value={tabpane.value}>
+        <TabsContent
+          key={tabpane.id}
+          value={tabpane.value}
+          className="mt-0 p-2 rounded-md rounded-tl-none"
+        >
           {tabpane.component}
         </TabsContent>
       ))}
