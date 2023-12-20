25cc33add569ce06c8f0e1ab3eda6ccf60b24c52
Amin Babu <amin.babu.bd@gmail.com>
2023-12-20 23:28:47 +0600

    Update max height in ScrollArea component

--- components/global/ScrollArea.jsx
+++ components/global/ScrollArea.jsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 const ScrollArea = ({ className, children }) => {
   return (
     <div
-      className={cn("overflow-y-auto overflow-x-hidden max-h-64", className)}
+      className={cn("overflow-y-auto overflow-x-hidden max-h-72", className)}
     >
       {children}
     </div>
