1f47c93d5b6a09fc326f371cf60df118d5c92727
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 02:24:17 +0600

    Update Tabs styling in Tabs.jsx

--- components/global/Tabs.jsx
+++ components/global/Tabs.jsx
@@ -9,13 +9,13 @@ import { cn } from "@/lib/utils";
 const Tabs = ({ data, defaultValue }) => {
   return (
     <TabsComponent defaultValue={defaultValue}>
-      <TabsList className="h-auto p-0 rounded-b-none rounded-t-md">
+      <TabsList className="h-auto p-0 rounded-t-md rounded-b-none flex md:inline-flex bg-white">
         {data.map((tab, index) => (
           <TabsTrigger
             key={tab.id}
             value={tab.value}
             className={cn(
-              "p-3 lg:px-6 lg:py-4 flex-col xs:flex-row gap-1 text-sm lg:text-lg text-t-700 bg-white rounded-none data-[state=active]:bg-p-900 data-[state=active]:text-white data-[state=active]:border-b-p-900",
+              "p-3 lg:px-6 lg:py-4 flex-col xs:flex-row gap-1 text-sm md:text-base lg:text-lg text-t-700 bg-white rounded-none data-[state=active]:bg-p-900 data-[state=active]:text-white data-[state=active]:border-b-p-900",
               {
                 "rounded-tl-md": index === 0,
                 "rounded-tr-md": index === data.length - 1,
