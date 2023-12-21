71610b2ea449aaf5fab0c75c395cb5905b45afeb
Amin Babu <amin.babu.bd@gmail.com>
2023-12-21 23:14:21 +0600

    Add search page layout and filters component

--- app/search/[slug]/page.jsx
+++ app/search/[slug]/page.jsx
@@ -1,7 +1,25 @@
-const SearchPage = ({ params }) => {
+import Container from "@/components/layouts/Container";
+import Image from "next/image";
+import BgCurve from "@/public/images/search/curve.svg?url";
+import Filters from "@/components/serach/Filters";
+
+const SearchPage = async ({ params }) => {
   const { slug } = params;
 
-  return <div>{slug}</div>;
+  // api call
+
+  return (
+    <div className="relative">
+      <Image
+        src={BgCurve}
+        alt="Bg curve"
+        className="absolute top-0 left-0 w-full -z-10 object-cover object-center"
+      />
+      <Container>
+        <Filters slug={slug} />
+      </Container>
+    </div>
+  );
 };
 
 export default SearchPage;
---
+++ components/serach/Filters.jsx
@@ -0,0 +1,19 @@
+import UmrahTabpane from "../global/UmrahTabpane";
+import { PencilIcon } from "@/components/icons/svgr";
+
+const Filters = ({ slug }) => {
+  switch (slug) {
+    case "umrah-flight":
+      return <h1>Umrah Flight</h1>;
+    case "umrah":
+      return <UmrahTabpane icon={<PencilIcon />} />;
+    case "visa":
+      return <h1>Visa</h1>;
+    case "customs":
+      return <h1>Customs</h1>;
+    default:
+      return null;
+  }
+};
+
+export default Filters;
