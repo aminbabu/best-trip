32e96ee3c979e0638b55b51a50c7682b351be932
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 02:00:22 +0600

    Update Hero.jsx to adjust margin on large screens

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <header className="bg-p-300/60 py-16">
       <Container>
         <div className="flex flex-col lg:flex-row items-center gap-x-6 gap-y-12 text-center lg:text-left">
-          <div className="max-w-[29rem] lg:max-w-lg mx-auto">
+          <div className="max-w-[29rem] lg:max-w-lg mx-auto lg:mx-0">
             <h1 className="text-3xl leading-tight sm:text-4xl sm:leading-tight lg:text-[2.75rem] lg:leading-tight text-t-800 font-medium mb-6">
               Discover A Beautiful Place With Us
             </h1>
