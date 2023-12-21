d7b330ff7e81e2b9b2fa8ea3b0ffbb9eb9646033
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 02:25:59 +0600

    Update icon sizes and component styles in Filters.jsx

--- components/home/Filters.jsx
+++ components/home/Filters.jsx
@@ -18,49 +18,41 @@ const data = [
     id: 1,
     icon: (
       <PlaneIcon
-        className="w-5 h-5 sm:w-6 sm:h-6 rotate-45"
+        className="w-5 h-5 md:w-6 md:h-6 rotate-45"
         viewBox="0 0 14 14"
       />
     ),
     title: "Umrah Flight",
     value: "umrah-flight",
-    component: (
-      <UmrahFlightTabpane className="rounded-t-none min-[618px]:rounded-tr-md min-h-[174px]" />
-    ),
+    component: <UmrahFlightTabpane className="rounded-tl-none min-h-[174px]" />,
   },
   {
     id: 2,
-    icon: <KabaIcon className="w-5 h-5 sm:w-6 sm:h-6" viewBox="0 0 26 26" />,
+    icon: <KabaIcon className="w-5 h-5 md:w-6 md:h-6" viewBox="0 0 26 26" />,
     title: "Umrah",
     value: "umrah",
-    component: (
-      <UmrahTabpane className="rounded-t-none min-[618px]:rounded-tr-md min-h-[174px]" />
-    ),
+    component: <UmrahTabpane className="rounded-tl-none min-h-[174px]" />,
   },
   {
     id: 3,
     icon: (
-      <PassportIcon className="w-5 h-5 sm:w-6 sm:h-6" viewBox="0 0 14 14" />
+      <PassportIcon className="w-5 h-5 md:w-6 md:h-6" viewBox="0 0 14 14" />
     ),
     title: "Visa",
     value: "visa",
-    component: (
-      <VisaTabpane className="rounded-t-none min-[618px]:rounded-tr-md min-h-[174px]" />
-    ),
+    component: <VisaTabpane className="rounded-tl-none min-h-[174px]" />,
   },
   {
     id: 4,
     icon: (
       <BuildingHumanIcon
-        className="w-5 h-5 sm:w-6 sm:h-6"
+        className="w-5 h-5 md:w-6 md:h-6"
         viewBox="0 0 26 26"
       />
     ),
     title: "Customs",
     value: "customs",
-    component: (
-      <CustomsTabpane className="rounded-t-none min-[618px]:rounded-tr-md min-h-[174px]" />
-    ),
+    component: <CustomsTabpane className="rounded-tl-none min-h-[174px]" />,
   },
 ];
 
