61d768df2255c88bdb7ad8b4fc847f05a2e46b75
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 12:06:49 +0600

    Refactor profile layout to use flexbox and add sticky sidebar

--- app/profile/layout.jsx
+++ app/profile/layout.jsx
@@ -4,8 +4,8 @@ import Sidebar from "@/components/profile/Sidebar";
 const ProfileLayout = ({ children }) => {
   return (
     <section className="py-12 lg:py-16 bg-primary-foreground">
-      <Container className="flex items-start gap-8">
-        <div className="sticky top-20 w-80">
+      <Container className="flex flex-col lg:flex-row lg:items-start gap-8">
+        <div className="lg:sticky lg:top-20 lg:w-80">
           <Sidebar />
         </div>
         <div className="flex-1">{children}</div>
