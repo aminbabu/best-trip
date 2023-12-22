ed21eebb0af90b7995ab904de9a69c83cf734698
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 14:14:22 +0600

    Update profile layout styles

--- app/profile/layout.jsx
+++ app/profile/layout.jsx
@@ -5,10 +5,10 @@ const ProfileLayout = ({ children }) => {
   return (
     <section className="py-12 lg:py-16 bg-primary-foreground">
       <Container className="flex flex-col lg:flex-row lg:items-start gap-8">
-        <div className="lg:sticky lg:top-20 lg:w-80">
+        <div className="lg:sticky lg:top-20 lg:w-80 bg-white">
           <Sidebar />
         </div>
-        <div className="flex-1">{children}</div>
+        <div className="flex-1 bg-white">{children}</div>
       </Container>
     </section>
   );
