535a7e633969dda3970d1f3857d0186d1eba686a
Amin Babu <amin.babu.bd@gmail.com>
2023-12-23 00:58:28 +0600

    Add profile password page component

---
+++ app/profile/password/page.jsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const formSchema = z
+  .object({
+    password: z.string().min(8, "Please enter a valid password"),
+    confirmPassword: z.string().min(8, "Please enter a valid password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+const ProfilePasswordPage = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Card className="border-transparent mb-8">
+      <CardHeader className="lg:p-10">
+        <CardTitle className="lg:text-[2rem] text-t-800 text-center pt-5">
+          Change Password
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="lg:p-10 lg:pt-0">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormLabel className="text-t-700 text-base lg:text-lg">
+                    New Password<span className="text-primary">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      className="h-[3.25rem] text-base px-5 py-4 text-t-700 placeholder:text-t-300"
+                      placeholder="Enter password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormLabel className="text-t-700 text-base lg:text-lg">
+                    Confirm Password<span className="text-primary">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      className="h-[3.25rem] text-base px-5 py-4 text-t-700 placeholder:text-t-300"
+                      placeholder="Enter confirm password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="grid">
+              <Button type="submit">Update Password</Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProfilePasswordPage;
