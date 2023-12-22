695dab66e09125e7f9f9c3a31203d36cecc2356f
Amin Babu <amin.babu.bd@gmail.com>
2023-12-23 00:18:17 +0600

    Refactor profile page layout

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -11,7 +11,7 @@ const MyProfilePage = () => {
 
   return (
     <div className="lg:p-6 xl:p-10 space-y-10">
-      <div className="text-center">
+      <div className="flex flex-col items-center">
         <Avatar className="h-40 w-40 mb-5 mx-auto">
           <AvatarImage src="/images/profile/avatar.png" />
           <AvatarFallback className="text-4xl">I.H</AvatarFallback>
@@ -19,16 +19,18 @@ const MyProfilePage = () => {
         <h1 className="text-t-800 text-2xl lg:text-[2rem] font-semibold mb-2.5">
           Irfanul Haque
         </h1>
-        <p className="text-base lg:text-lg text-t-600 mb-5">
+        <p className="text-base lg:text-lg text-t-600">
           mdirfanulhaque2020@gmail.com
         </p>
         {!edit && (
-          <Button size="sm" onClick={() => setEdit(true)}>
-            Edit Profile
-            <PencilSquareIcon />
-          </Button>
+          <div className="mt-5">
+            <Button size="sm" onClick={() => setEdit(true)}>
+              Edit Profile
+              <PencilSquareIcon />
+            </Button>
+          </div>
         )}
-        <div className="flex justify-center gap-x-1 text-base lg:text-lg text-t-600 leading-relaxed mt-8">
+        <div className="flex items-start justify-center gap-x-1 text-base lg:text-lg text-t-600 leading-relaxed mt-8">
           <span className="text-primary inline-flex items-center justify-center">
             <ICircleIcon />
           </span>
