4d8323137ae7281c54336c27f656727fec6c2989
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 11:57:25 +0600

    Refactor profile route

--- components/profile/Sidebar.jsx
+++ components/profile/Sidebar.jsx
@@ -1,5 +1,55 @@
+"use client";
+
+import Link from "next/link";
+import {
+  CalenderCheckIcon,
+  CreditCardIcon,
+  LogoutIcon,
+  SecurityLockIcon,
+  UserIcon,
+} from "@/components/icons/svgr";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navLinks = [
+  { name: "Profile", href: "/profile", icon: <UserIcon /> },
+  { name: "My Booking", href: "/profile/booking", icon: <CalenderCheckIcon /> },
+  {
+    name: "Payment History",
+    href: "/profile/payment",
+    icon: <CreditCardIcon />,
+  },
+  {
+    name: "Change Password",
+    href: "/profile/password",
+    icon: <SecurityLockIcon />,
+  },
+  { name: "Logout", href: "/logout", icon: <LogoutIcon /> },
+];
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const pathname = usePathname();
+
+  return (
+    <ul>
+      {navLinks.map((link, index) => (
+        <li key={index}>
+          <Link
+            href={link.href}
+            className={cn(
+              "flex items-center gap-x-3 p-4 lg:p-6 text-base lg:text-lg text-t-700 leading-relaxed duration-300 hover:bg-primary hover:text-white",
+              {
+                "bg-primary text-white": pathname === link.href,
+              }
+            )}
+          >
+            <span>{link.icon}</span>
+            <span>{link.name}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Sidebar;
