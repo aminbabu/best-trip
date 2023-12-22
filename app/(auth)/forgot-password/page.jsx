bf0a48252b3ad6248afb381cd60b91238b52279d
Amin Babu <amin.babu.bd@gmail.com>
2023-12-22 11:10:39 +0600

    Add forgot password page component

---
+++ app/(auth)/forgot-password/page.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import validator from "validator";
+import Link from "next/link";
+import { CheckIcon } from "lucide-react";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+});
+
+const ForgotPasswordPage = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="max-w-[648px] mx-auto">
+      <Card className="border-transparent mb-8">
+        <CardHeader className="lg:p-10">
+          <CardTitle className="lg:text-[2rem] text-t-800 text-center pt-5">
+            Enter your email
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="lg:p-10 lg:pt-0">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="space-y-3">
+                    <FormLabel className="text-t-700 text-base lg:text-lg">
+                      Email address<span className="text-primary">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        className="h-[3.25rem] text-t-700 placeholder:text-t-300"
+                        placeholder="Enter email address"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="grid">
+                <Button type="submit">Send</Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
