b5e9251d96b655723e4514d6a8396ee5c3a631f2
Amin Babu <amin.babu.bd@gmail.com>
2023-12-29 21:53:50 +0600

    Update traveller field validation to expect a number

--- schema/zod.js
+++ schema/zod.js
@@ -4,5 +4,5 @@ export const umrahSchema = z.object({
   schedule: z.string().min(1, { message: "Schedule is required" }),
   type: z.string().min(1, { message: "Package is required" }),
   duration: z.string().min(1, { message: "Duration is required" }),
-  traveller: z.string().min(1, { message: "Traveller is required" }),
+  traveller: z.number().min(1, { message: "Traveller is required" }),
 });
