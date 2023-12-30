e3aa240c81cfb09b260d5d84e34181570d0d777d
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 14:59:57 +0600

    Refactor Umrah package overview component

--- components/umrah/Overview.jsx
+++ components/umrah/Overview.jsx
@@ -41,108 +41,103 @@ const Overview = () => {
 
   return (
     <section className="umrah-package-overview">
-      <Container>
-        <Card className="border-transparent">
-          <CardContent className="grid grid-cols-2 gap-y-8 p-4 sm:p-6 lg:p-8">
-            <div className="col-span-2 lg:col-span-1 space-y-6">
-              <Slider hasTrack={false} options={options}>
-                <SplideTrack>
-                  {items.map((item, index) => (
-                    <SplideSlide key={index}>
-                      <div className="aspect-square overflow-hidden">
-                        <Image
-                          src={item.image}
-                          alt={item.title}
-                          width={266}
-                          height={266}
-                          className="w-full h-full object-cover"
-                        />
-                      </div>
-                    </SplideSlide>
-                  ))}
-                </SplideTrack>
-              </Slider>
+      <Card className="border-transparent">
+        <CardContent className="grid grid-cols-2 gap-y-8 p-4 sm:p-6 lg:p-8">
+          <div className="col-span-2 lg:col-span-1 space-y-6">
+            <Slider hasTrack={false} options={options}>
+              <SplideTrack>
+                {items.map((item, index) => (
+                  <SplideSlide key={index}>
+                    <div className="aspect-square overflow-hidden">
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        width={266}
+                        height={266}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  </SplideSlide>
+                ))}
+              </SplideTrack>
+            </Slider>
+          </div>
+          <div className="col-span-2 lg:col-span-1 space-y-9 lg:pl-12">
+            <div>
+              <h1 className="text-xl lg:text-2xl text-t-900 font-medium mb-2.5">
+                Quad Share Basis Package
+              </h1>
+              <p className="text-t-800">
+                1 Friday In Makkah- 1 Friday In Madinah
+              </p>
             </div>
-            <div className="col-span-2 lg:col-span-1 space-y-9 lg:pl-12">
-              <div>
-                <h1 className="text-xl lg:text-2xl text-t-900 font-medium mb-2.5">
-                  Quad Share Basis Package
-                </h1>
-                <p className="text-t-800">
-                  1 Friday In Makkah- 1 Friday In Madinah
-                </p>
-              </div>
-              <div className="flex flex-col gap-y-9">
-                <ul className="space-y-3 lg:space-y-2 xl:space-y-3">
-                  <li className="flex gap-x-2 text-sm lg:text-base text-t-600 leading-normal">
-                    <LocationCircleIcon
-                      className="mt-0.5 flex-shrink-0 h-6 w-6 text-primary"
-                      viewBox="0 0 16 17"
+            <div className="flex flex-col gap-y-9">
+              <ul className="space-y-3 lg:space-y-2 xl:space-y-3">
+                <li className="flex gap-x-2 text-sm lg:text-base text-t-600 leading-normal">
+                  <LocationCircleIcon
+                    className="mt-0.5 flex-shrink-0 h-6 w-6 text-primary"
+                    viewBox="0 0 16 17"
+                  />
+                  <span className="flex-shrink-0">
+                    From <span className="text-t-900">Dhaka , Bangladesh</span>
+                  </span>
+                </li>
+                <li className="flex gap-x-2 text-sm lg:text-base text-t-600 leading-normal">
+                  <ClockAltIcon
+                    className="mt-0.5 flex-shrink-0 h-6 w-6 text-primary"
+                    viewBox="0 0 16 17"
+                  />
+                  <span className="flex-shrink-0">
+                    15 Days | <span className="text-t-900">14 Nights</span>
+                  </span>
+                </li>
+                <li className="flex gap-x-2 text-sm lg:text-base text-t-600 leading-normal">
+                  <PeopleIcon
+                    className="mt-0.5 flex-shrink-0 h-6 w-6 text-primary"
+                    viewBox="0 0 16 17"
+                  />
+                  <span className="flex-shrink-0">Group Available 30 Pax</span>
+                </li>
+              </ul>
+              <div className="flex flex-col gap-y-4">
+                <div className="text-t-700 text-base lg:text-lg leading-normal">
+                  Inclusions
+                </div>
+                <ul className="flex items-center gap-x-4">
+                  <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
+                    <PlaneIcon
+                      className="w-6 h-6 rotate-45 text-primary"
+                      viewBox="0 0 14 14"
                     />
-                    <span className="flex-shrink-0">
-                      From{" "}
-                      <span className="text-t-900">Dhaka , Bangladesh</span>
-                    </span>
+                    Flight
                   </li>
-                  <li className="flex gap-x-2 text-sm lg:text-base text-t-600 leading-normal">
-                    <ClockAltIcon
-                      className="mt-0.5 flex-shrink-0 h-6 w-6 text-primary"
-                      viewBox="0 0 16 17"
+                  <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
+                    <HotelIcon
+                      className="w-6 h-6 text-primary"
+                      viewBox="0 0 14 14"
                     />
-                    <span className="flex-shrink-0">
-                      15 Days | <span className="text-t-900">14 Nights</span>
-                    </span>
+                    Hotel
                   </li>
-                  <li className="flex gap-x-2 text-sm lg:text-base text-t-600 leading-normal">
-                    <PeopleIcon
-                      className="mt-0.5 flex-shrink-0 h-6 w-6 text-primary"
-                      viewBox="0 0 16 17"
+                  <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
+                    <PassportIcon
+                      className="w-6 h-6 text-primary"
+                      viewBox="0 0 14 14"
                     />
-                    <span className="flex-shrink-0">
-                      Group Available 30 Pax
-                    </span>
+                    Visa
+                  </li>
+                  <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
+                    <BusIcon
+                      className="w-6 h-6 text-primary"
+                      viewBox="0 0 14 14"
+                    />
+                    Transport
                   </li>
                 </ul>
-                <div className="flex flex-col gap-y-4">
-                  <div className="text-t-700 text-base lg:text-lg leading-normal">
-                    Inclusions
-                  </div>
-                  <ul className="flex items-center gap-x-4">
-                    <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
-                      <PlaneIcon
-                        className="w-6 h-6 rotate-45 text-primary"
-                        viewBox="0 0 14 14"
-                      />
-                      Flight
-                    </li>
-                    <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
-                      <HotelIcon
-                        className="w-6 h-6 text-primary"
-                        viewBox="0 0 14 14"
-                      />
-                      Hotel
-                    </li>
-                    <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
-                      <PassportIcon
-                        className="w-6 h-6 text-primary"
-                        viewBox="0 0 14 14"
-                      />
-                      Visa
-                    </li>
-                    <li className="flex flex-col items-center gap-y-1 text-xs lg:text-sm text-t-900 capitalize">
-                      <BusIcon
-                        className="w-6 h-6 text-primary"
-                        viewBox="0 0 14 14"
-                      />
-                      Transport
-                    </li>
-                  </ul>
-                </div>
               </div>
             </div>
-          </CardContent>
-        </Card>
-      </Container>
+          </div>
+        </CardContent>
+      </Card>
     </section>
   );
 };
