e48a8a1c168e5181b8e96c0d6e9081dec4e6f3f6
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 12:12:47 +0600

    Update import path for Container component

--- components/umrah/DetailsTab.jsx
+++ components/umrah/DetailsTab.jsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import Container from "../layouts/Container";
+import Container from "@/components/layouts/Container";
 
 const DetailsTab = ({ id }) => {
   const pathname = usePathname();
