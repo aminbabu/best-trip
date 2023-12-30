7fdac188a8ce71b068b987e8e1e7daac89aa08d9
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 22:58:31 +0600

    Refactor MessageCard component styling

--- components/global/MessageCard.jsx
+++ components/global/MessageCard.jsx
@@ -13,8 +13,13 @@ const MessageCard = ({
   className,
 }) => {
   return (
-    <Card className={cn("", className)}>
-      <CardContent className="text-center px-6 py-8 lg:px-12 lg:py-14">
+    <Card
+      className={cn(
+        "max-w-3xl mx-auto border-transparent text-t-700 leading-relaxed",
+        className
+      )}
+    >
+      <CardContent className="text-center p-4 sm:p-6 md:p-8 lg:p-10">
         {icon && <div className="mb-8">{icon}</div>}
         {title && (
           <h4 className="text-t-800 text-2xl lg:text-[2rem] font-semibold mb-4">
@@ -25,7 +30,7 @@ const MessageCard = ({
           <p className="text-base lg:text-lg mb-4">{description}</p>
         )}
         <div className="mb-6">{children}</div>
-        <Button className="w-80" asChild>
+        <Button className="w-72 sm:w-80" asChild>
           <Link href={href || "/"}>{buttonText || "Home"}</Link>
         </Button>
       </CardContent>
