cf55a03bdd39a32fb4efc2b33e99e354f9787ac5
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 20:35:41 +0600

    Add traveler details form with information tooltip

--- components/umrah/traveller-details/TravellerDetailsForm.jsx
+++ components/umrah/traveller-details/TravellerDetailsForm.jsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { InfoIcon } from "lucide-react";
+
 const TravellerDetailsForm = () => {
-  return <div>TravellerDetailsForm</div>;
+  return (
+    <div className="space-y-8">
+      <h2 className="text-t-700 text-lg font-medium">Traveler Details</h2>
+
+      <div className="bg-p-300 font-normal lg:text-lg text-t-800 rounded-sm px-4 py-3 flex items-center gap-x-2.5">
+        <InfoCircledIcon className="w-6 h-6 text-p-900 flex-shrink-0" />
+        Enter details exactly as they appear on your passport
+      </div>
+    </div>
+  );
 };
 
 export default TravellerDetailsForm;
