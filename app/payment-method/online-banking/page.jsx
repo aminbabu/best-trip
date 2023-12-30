665714482e9623e4f6f37b2e8ecf142aeffb6901
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 23:28:37 +0600

    Add OnlineBankingPage component

---
+++ app/payment-method/online-banking/page.jsx
@@ -0,0 +1,27 @@
+import MessageCard from "@/components/global/MessageCard";
+import { LoaderIcon } from "@/components/icons/svgr";
+import Container from "@/components/layouts/Container";
+
+const OnlineBankingPage = () => {
+  return (
+    <main className="py-20 bg-secondary">
+      <Container>
+        <MessageCard
+          title="Please Wait..."
+          description="Your payment is currently being processed "
+          icon={
+            <div className="grid place-items-center">
+              <LoaderIcon className="animate-[spin_2s_linear_infinite] text-p-900" />
+            </div>
+          }
+        >
+          <p className="lg:text-lg text-primary font-medium">
+            Don’t close the browser
+          </p>
+        </MessageCard>
+      </Container>
+    </main>
+  );
+};
+
+export default OnlineBankingPage;
