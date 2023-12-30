6643d777bb6591d281387f25982a81e094495ac0
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 20:46:54 +0600

    Refactor DataTableToolbar component

--- components/umrah/traveller-details/data-table-toolbar.jsx
+++ components/umrah/traveller-details/data-table-toolbar.jsx
@@ -11,23 +11,25 @@ const DataTableToolbar = ({ table, showTravellerForm }) => {
   };
 
   return (
-    <div className="flex flex-wrap items-center justify-between gap-x-3 lg:gap-x-5 gap-y-4">
-      <h3 className="basis-full sm:basis-auto text-base lg:text-lg text-t-700 font-medium">
+    <div className="flex flex-col md:flex-row md:items-center justify-between gap-x-4 lg:gap-x-5 gap-y-4">
+      <h3 className="text-base lg:text-lg text-t-700 font-medium">
         Traveler Management ({table.getCoreRowModel()?.rows?.length})
       </h3>
-      <div className="relative ml-auto text-t-700 flex-1 sm:flex-grow-0 sm:flex-shrink-0">
-        <SearchIcon className="absolute top-1/2 -translate-y-1/2 left-3 pointer-events-none" />
-        <Input
-          placeholder="Search by name"
-          value={table.getColumn("name")?.getFilterValue() ?? ""}
-          onChange={handleChange}
-          className="sm:w-[272px] py-4 pl-12"
-        />
+      <div className="flex gap-x-4">
+        <div className="flex-1 relative text-t-700">
+          <SearchIcon className="absolute top-1/2 -translate-y-1/2 left-3 pointer-events-none" />
+          <Input
+            placeholder="Search by name"
+            value={table.getColumn("name")?.getFilterValue() ?? ""}
+            onChange={handleChange}
+            className="w-full md:w-[272px] py-4 pl-12"
+          />
+        </div>
+        <Button className="font-semibold" onClick={showTravellerForm}>
+          <PlusCircleIcon />
+          <span className="hidden sm:inline-block">Add Traveler</span>
+        </Button>
       </div>
-      <Button className="font-semibold" onClick={showTravellerForm}>
-        <PlusCircleIcon />
-        Add Traveler
-      </Button>
     </div>
   );
 };
