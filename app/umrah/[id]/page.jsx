5db0237603bb0a02d365169dc2856b95bb093d8f
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 11:07:50 +0600

    Update UmrahDetailsPage and Overview components

--- app/umrah/[id]/page.jsx
+++ app/umrah/[id]/page.jsx
@@ -4,7 +4,7 @@ const UmrahDetailsPage = ({ params }) => {
   const { id } = params;
 
   return (
-    <main className="bg-secondary py-20 space-y-6">
+    <main className="bg-secondary py-10 xs:py-12 sm:py-14 md:py-16 lg:py-20 space-y-6">
       <Overview />
     </main>
   );
