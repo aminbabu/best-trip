639d4ff82cf4a261ac7d9d756c06fab08c6bda92
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 17:07:55 +0600

    Add Umrah terms and conditions page component

---
+++ app/umrah/[id]/terms-conditions/page.jsx
@@ -0,0 +1,114 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+const ItineraryPage = () => {
+  return (
+    <div className="space-y-4">
+      <Card className="border-transparent">
+        <CardContent className="space-y-8 p-4 sm:p-6 lg:p-8">
+          <div className="space-y-8">
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-lg text-t-800 font-medium mb-3.5">
+                How do I enter into the state of Ihram?
+              </h2>
+              <p className="text-t-600">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry&apos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book. It
+                has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised.
+              </p>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default ItineraryPage;
