d3e8cae4f55e7adbf9f33c0c70376797bb44ccc8
Amin Babu <amin.babu.bd@gmail.com>
2023-12-30 11:41:42 +0600

    Add umrahSchema and uuidSchema to zod.js

--- schema/zod.js
+++ schema/zod.js
@@ -1,8 +1,20 @@
 import { z } from "zod";
 
+/**
+ * @description Schema for umrah search
+ *
+ */
 export const umrahSchema = z.object({
   schedule: z.string().min(1, { message: "Schedule is required" }),
   type: z.string().min(1, { message: "Package is required" }),
   duration: z.string().min(1, { message: "Duration is required" }),
   traveller: z.number().min(1, { message: "Traveller is required" }),
+});
+
+/**
+ * @description Schema for uuid validation
+ *
+ */
+export const uuidSchema = z.object({
+  uuid: z.string().uuid({ message: "Invalid uuid" }),
 });
