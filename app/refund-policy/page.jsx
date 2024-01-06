5d3f95d421a957edc18e3a594aa069fbbbe27730
Amin Babu <amin.babu.bd@gmail.com>
2024-01-06 22:55:50 +0600

    Add refund policy page component

---
+++ app/refund-policy/page.jsx
@@ -0,0 +1,36 @@
+import Container from "@/components/layouts/Container";
+import data from "@/data/qna.json";
+
+const RefundPolicyPage = () => {
+  return (
+    <main>
+      <section className="py-12 lg:py-16">
+        <Container>
+          <ul className="space-y-8">
+            <li className="space-y-3.5">
+              <h2 className="text-lg text-t-800 font-semibold">
+                Refund Policy
+              </h2>
+              <p className="text-t-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Adipisci, unde officiis, voluptatibus facilis distinctio vel
+                corporis vero a, nesciunt cum accusamus. Placeat labore debitis
+                officia! Ab laborum magni nihil accusantium!
+              </p>
+            </li>
+            {data?.map((item, index) => (
+              <li key={index} className="space-y-3.5">
+                <h2 className="text-lg text-t-800 font-semibold">
+                  {item.question}
+                </h2>
+                <p className="text-t-600">{item.answer}</p>
+              </li>
+            ))}
+          </ul>
+        </Container>
+      </section>
+    </main>
+  );
+};
+
+export default RefundPolicyPage;
