255d5587cb6c57c16da1c51bc74b3e85270a35d0
Amin Babu <amin.babu.bd@gmail.com>
2024-01-06 22:54:50 +0600

    Add privacy policy section to the page

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -7,6 +7,17 @@ const PrivaryPolicyPage = () => {
       <section className="py-12 lg:py-16">
         <Container>
           <ul className="space-y-8">
+            <li className="space-y-3.5">
+              <h2 className="text-lg text-t-800 font-semibold">
+                Privacy Policy
+              </h2>
+              <p className="text-t-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Adipisci, unde officiis, voluptatibus facilis distinctio vel
+                corporis vero a, nesciunt cum accusamus. Placeat labore debitis
+                officia! Ab laborum magni nihil accusantium!
+              </p>
+            </li>
             {data?.map((item, index) => (
               <li key={index} className="space-y-3.5">
                 <h2 className="text-lg text-t-800 font-semibold">
