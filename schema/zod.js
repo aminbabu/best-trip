bcbff4d6040dd503feface637f48d4e698ec57f5
Amin Babu <amin.babu.bd@gmail.com>
2024-01-07 22:05:16 +0600

    Refactor umrahSchema in zod.js

--- schema/zod.js
+++ schema/zod.js
@@ -8,8 +8,11 @@ import { z } from "zod";
 export const umrahSchema = z.object({
   schedule: z.string().min(1, { message: "Schedule is required" }),
   type: z.string().min(1, { message: "Package is required" }),
-  duration: z.string().min(1, { message: "Duration is required" }),
-  traveller: z.number().min(1, { message: "Traveller is required" }),
+  duration: z
+    .string()
+    .min(1, { message: "Duration is required" })
+    .or(z.number().min(1, { message: "Duration is required" })),
+  travellers: z.number().min(1, { message: "Traveller is required" }),
 });
 
 /**
