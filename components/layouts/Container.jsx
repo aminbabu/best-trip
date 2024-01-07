418762cb0264890cd41821cf821b8b8e70203698
Amin Babu <amin.babu.bd@gmail.com>
2024-01-07 22:08:31 +0600

    Update Container component styling

--- components/layouts/Container.jsx
+++ components/layouts/Container.jsx
@@ -2,7 +2,12 @@ import { cn } from "@/lib/utils";
 
 const Container = ({ className, children }) => {
   return (
-    <div className={cn("max-w-7xl mx-auto px-5 lg:px-12 xl:px-16", className)}>
+    <div
+      className={cn(
+        "max-w-[calc(1280px_-_12px)] mx-auto px-5 lg:px-12 xl:px-16",
+        className
+      )}
+    >
       {children}
     </div>
   );
