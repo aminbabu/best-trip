e52ce03060ca7460fabcbc2c85d703f4f5f380d6
Amin Babu <amin.babu.bd@gmail.com>
2024-01-07 23:11:29 +0600

    Update Section component styling

--- components/global/Section.jsx
+++ components/global/Section.jsx
@@ -3,7 +3,9 @@ import Container from "../layouts/Container";
 
 const Section = ({ className, children }) => {
   return (
-    <section className={cn("py-12 sm:py-16 lg:py-20 xl:py-24", className)}>
+    <section
+      className={cn("py-12 sm:py-16 lg:py-20 xl:py-[5.625rem]", className)}
+    >
       <Container>{children}</Container>
     </section>
   );
