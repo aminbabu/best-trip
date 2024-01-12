8fdb65396c11cbcde033a244ac43d023b306d4cd
Amin Babu <amin.babu.bd@gmail.com>
2024-01-12 23:47:43 +0600

    Update PopularDestinationSlideItem styles

--- components/home/PopularDestinationSlideItem.jsx
+++ components/home/PopularDestinationSlideItem.jsx
@@ -18,8 +18,8 @@ const PopularDestinationSlideItem = ({ item }) => {
           className="rounded-t-md object-cover w-full h-full duration-300 group-hover:scale-110"
         />
       </Link>
-      <div className="flex flex-col gap-y-4 p-2.5 pt-0">
-        <h3 className="text-lg font-medium leading-tight">
+      <div className="flex flex-col gap-y-1.5 lg:gap-y-4 p-2.5 pt-0">
+        <h3 className="text-base lg:text-lg font-medium leading-tight">
           <Link
             href="#"
             className="text-t-800 duration-300 hover:text-p-900 flex items-center gap-x-2.5 line-clamp-1"
@@ -32,11 +32,11 @@ const PopularDestinationSlideItem = ({ item }) => {
           </Link>
         </h3>
         <div className="flex items-center gap-x-1.5">
-          <p className="flex-1 text-t-600 text-sm line-clamp-1">
+          <p className="flex-1 text-t-600 text-xs lg:text-sm line-clamp-1">
             {item?.airline}
           </p>
         </div>
-        <div className="flex items-center justify-between text-sm gap-x-4 leading-relaxed">
+        <div className="flex items-center justify-between gap-x-4 leading-relaxed text-xs lg:text-sm">
           <p className="text-t-600">Starting from </p>
           <p className="text-p-900 font-medium">
             {numeral(item?.price).format("$0,0.00")}
