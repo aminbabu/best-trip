217d05423b118ec58f57d5f9879c53a4e0686088
Amin Babu <amin.babu.bd@gmail.com>
2024-01-12 21:12:47 +0600

    Refactor Hero component layout and styles

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -5,17 +5,17 @@ const Hero = () => {
   return (
     <header className="bg-p-300/60 py-12 sm:py-16">
       <Container>
-        <div className="flex flex-col lg:flex-row items-center gap-x-6 gap-y-12">
-          <div className="max-w-[29rem] lg:max-w-lg mx-auto lg:mx-0">
-            <h1 className="text-2xl leading-tight sm:text-4xl sm:leading-tight lg:text-[2.75rem] lg:leading-tight text-t-800 font-medium mb-6">
-              Discover A Beautiful <br className="sm:hidden" /> Place With Us
+        <div className="flex flex-col lg:flex-row lg:items-center gap-x-6 gap-y-12">
+          <div className="lg:max-w-lg">
+            <h1 className="text-2xl xs:text-3xl leading-tight sm:text-4xl sm:leading-tight xl:text-[2.75rem] xl:leading-tight text-t-800 font-medium mb-2.5 sm:mb-6">
+              Discover A Beautiful <br className="md:hidden" /> Place With Us
             </h1>
-            <p className="text-base lg:text-lg text-t-600">
+            <p className="xl:text-lg text-t-600 max-w-xl">
               Would you explore nature paradise in the world, lets find the best
               destination in the world with us.
             </p>
           </div>
-          <div className="flex-shrink-0">
+          <div className="flex-shrink-0 hidden lg:block ml-auto">
             <Image
               src="/images/home/hero/illustration.png"
               alt="Luggage"
