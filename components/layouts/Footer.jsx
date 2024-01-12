197d353d196928a273925033948eeb6af3fbf6a6
Amin Babu <amin.babu.bd@gmail.com>
2024-01-12 20:10:06 +0600

    Update import paths for Footer components

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Brand from "@/components/global/Brand";
-import Container from "@/components/Container";
-import Copyright from "@/components/Copyright";
-import Newsletter from "@/components/Newsletter";
+import Container from "@/components/layouts/Container";
+import Copyright from "@/components/layouts/Copyright";
+import Newsletter from "@/components/layouts/Newsletter";
 import {
   FacebookIcon,
   InstagramIcon,
