a8309089d64d5dd6893501f76db6556a82cf3a7d
Amin Babu <amin.babu.bd@gmail.com>
2024-01-13 19:19:56 +0600

    Update mobile reponsive styles

--- components/layouts/Footer.jsx
+++ components/layouts/Footer.jsx
@@ -70,19 +70,19 @@ const socialMedia = {
 
 const Footer = ({ className }) => {
   return (
-    <footer className={cn("bg-secondary mt-auto", className)}>
+    <footer className={cn("bg-secondary mt-auto pt-10 lg:pt-0", className)}>
       <Container>
         <Newsletter />
         <div className="space-y-10">
-          <div className="grid grid-cols-4 gap-12 text-t-600">
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs lg:max-w-[264px] mx-auto lg:mx-0 col-span-4 lg:col-span-1 lg:-mr-11">
+          <div className="grid grid-cols-4 gap-x-5 gap-y-10 lg:gap-12 text-t-600">
+            <div className="flex flex-col max-w-sm lg:max-w-[264px] col-span-4 lg:col-span-1 lg:-mr-11">
               <Brand
                 logo="/images/brand-logo.png"
                 width="132"
                 height="56"
                 alt="Best Trip"
               />
-              <p className="text-base lg:text-lg mt-6">
+              <p className="xl:text-lg leading-relaxed xl:leading-relaxed mt-6">
                 <span className="text-p-900">Best Trip</span> Lorem Ipsum is
                 simply dummy text of the printing and typesetting industry.
                 Lorem{" "}
@@ -94,11 +94,11 @@ const Footer = ({ className }) => {
                 </Link>
               </p>
             </div>
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs mx-auto lg:mx-0 col-span-4 lg:col-span-1 lg:ml-11">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+            <div className="flex flex-col col-span-2 lg:col-span-1 lg:ml-11">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 {menu.title}
               </h2>
-              <ul className="grid gap-2">
+              <ul className="grid gap-y-1.5 lg:gap-y-2">
                 {menu.links.map((item, index) => (
                   <li key={index}>
                     <Link
@@ -111,17 +111,20 @@ const Footer = ({ className }) => {
                 ))}
               </ul>
             </div>
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs mx-auto lg:mx-0 col-span-4 lg:col-span-1">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+            <div className="flex flex-col col-span-2 lg:col-span-1">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 Accpeted Payment
               </h2>
-              <PaymentMethodIcons />
+              <PaymentMethodIcons
+                viewBox="0 0 213 71"
+                className="max-w-full h-auto"
+              />
             </div>
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs mx-auto lg:mx-0 col-span-4 lg:col-span-1">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+            <div className="flex flex-col col-span-2 lg:col-span-1">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 {socialMedia.title}
               </h2>
-              <ul className="grid gap-3 mb-3">
+              <ul className="grid gap-y-1.5 lg:gap-y-2 mb-1.5 lg:mb-2">
                 <li className="inline-flex">
                   <Link
                     href={`mailto:${socialMedia.email}`}
@@ -146,7 +149,7 @@ const Footer = ({ className }) => {
                   <li key={index}>
                     <Link
                       href={item.path}
-                      className="text-t-800 duration-300 hover:text-p-900 inline-flex items-center justify-center w-8 h-8 p-1.5"
+                      className="text-t-700 duration-300 hover:text-p-900 inline-flex items-center justify-center w-6 xs:w-7 lg:w-8 h-6 xs:h-7 lg:h-8 p-0.5"
                       title={item.name}
                     >
                       {item.icon}
@@ -155,45 +158,59 @@ const Footer = ({ className }) => {
                 ))}
               </ul>
             </div>
-          </div>
-          <hr className="border-[#d7d7d7]/50" />
-          <div className="grid grid-cols-4 gap-12 text-t-600">
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs lg:max-w-[264px] mx-auto lg:mx-0 col-span-4 lg:col-span-1 lg:-mr-11">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+            <div className="flex lg:hidden flex-col col-span-2 lg:col-span-1">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 Office Address
               </h2>
-              <p className="text-base lg:text-lg">
-                Rajlazmi Complex, Lift 6 <br />
-                Rajlaxmi, Uttara Sector 03 <br />
-                Dhaka, Bangladesh.
+              <p className="xl:text-lg leading-relaxed xl:leading-relaxed">
+                Rajlazmi Complex, Lift 6 Rajlaxmi, Uttara Sector 03 Dhaka,
+                Bangladesh.
               </p>
             </div>
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs mx-auto lg:mx-0 col-span-4 lg:col-span-1 lg:ml-11">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+          </div>
+          <hr className="border-[#d7d7d7]/50" />
+          <div className="grid grid-cols-4 sm:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-10 lg:gap-12  text-t-600">
+            <div className="hidden lg:flex flex-col max-w-xs lg:max-w-[264px] col-span-4 lg:col-span-1 lg:-mr-11">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
+                Office Address
+              </h2>
+              <p className="xl:text-lg leading-relaxed xl:leading-relaxed">
+                Rajlazmi Complex, Lift 6 Rajlaxmi, Uttara Sector 03 Dhaka,
+                Bangladesh.
+              </p>
+            </div>
+            <div className="flex flex-col items-center sm:items-start text-center sm:text-left col-span-2 sm:col-span-1 lg:ml-11">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 Authorised by
               </h2>
-              <ul className="flex items-center gap-4">
+              <ul className="flex items-center sm:ml-8">
                 <li className="inline-flex">
                   <Link
                     href="#"
-                    className="duration-300 hover:opacity-75 inline-flex py-0.5 ml-8"
+                    className="duration-300 hover:opacity-75 inline-flex py-0.5"
                   >
-                    <IataLogo />
+                    <IataLogo
+                      viewBox="0 0 52 40"
+                      className="w-10 h-auto lg:w-14"
+                    />
                   </Link>
                 </li>
               </ul>
             </div>
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs mx-auto lg:mx-0 col-span-4 lg:col-span-1">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+            <div className="flex flex-col items-center sm:items-start text-center sm:text-left col-span-2 sm:col-span-1">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 Member of
               </h2>
-              <ul className="flex items-center gap-4">
+              <ul className="flex items-center gap-x-2.5 lg:gap-x-4">
                 <li className="inline-flex">
                   <Link
                     href="#"
                     className="duration-300 hover:opacity-75 inline-flex py-0.5"
                   >
-                    <AtabLogo />
+                    <AtabLogo
+                      viewBox="0 0 97 26"
+                      className="w-auto h-[1.125rem] lg:h-[1.625rem]"
+                    />
                   </Link>
                 </li>
                 <li className="inline-flex">
@@ -201,22 +218,28 @@ const Footer = ({ className }) => {
                     href="#"
                     className="duration-300 hover:opacity-75 inline-flex py-0.5"
                   >
-                    <ToabLogo />
+                    <ToabLogo
+                      viewBox="0 0 62 26"
+                      className="w-auto h-[1.125rem] lg:h-[1.625rem]"
+                    />
                   </Link>
                 </li>
               </ul>
             </div>
-            <div className="flex flex-col items-center lg:items-start text-center lg:text-left max-w-xs mx-auto lg:mx-0 col-span-4 lg:col-span-1">
-              <h2 className="text-base lg:text-lg text-inherit font-medium mb-5">
+            <div className="flex flex-col items-center sm:items-start text-center sm:text-left col-span-4 sm:col-span-1">
+              <h2 className="text-xs xs:text-sm sm:text-base lg:text-lg text-inherit font-medium mb-3 lg:mb-5">
                 Accredited Member
               </h2>
-              <ul className="flex items-center gap-4">
+              <ul className="flex items-center gap-x-2.5 lg:gap-x-4">
                 <li className="inline-flex">
                   <Link
                     href="#"
                     className="duration-300 hover:opacity-75 inline-flex py-0.5"
                   >
-                    <BasisLogo />
+                    <BasisLogo
+                      viewBox="0 0 105 43"
+                      className="w-auto h-10 lg:h-12"
+                    />
                   </Link>
                 </li>
                 <li className="inline-flex">
@@ -224,7 +247,10 @@ const Footer = ({ className }) => {
                     href="#"
                     className="duration-300 hover:opacity-75 inline-flex py-0.5"
                   >
-                    <EcabLogo />
+                    <EcabLogo
+                      viewBox="0 0 53 31"
+                      className="w-auto h-6 lg:h-10"
+                    />
                   </Link>
                 </li>
               </ul>
