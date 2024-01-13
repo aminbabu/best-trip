7fc707e6b676b99657465ba7d19feea153c7074f
Amin Babu <amin.babu.bd@gmail.com>
2024-01-13 19:57:04 +0600

    Update Filters component

--- components/home/Filters.jsx
+++ components/home/Filters.jsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/icons/svgr";
 import Container from "@/components/layouts/Container";
 import UmrahTabpane from "@/components/global/UmrahTabpane";
-import BgCurve from "@/public/images/home/filters/curve.svg?url";
+import BgCurve from "@/public/images/home/filters/curve.svg";
 import Image from "next/image";
 import VisaTabpane from "@/components/global/VisaTabpane";
 import CustomsTabpane from "@/components/global/CustomsTabpane";
@@ -24,14 +24,18 @@ const data = [
     ),
     title: "Umrah Flight",
     value: "umrah-flight",
-    component: <UmrahFlightTabpane className="rounded-tl-none min-h-[174px]" />,
+    component: (
+      <UmrahFlightTabpane className="rounded-t-none sm:rounded-tr-md min-h-[174px]" />
+    ),
   },
   {
     id: 2,
     icon: <KabaIcon className="w-5 h-5 md:w-6 md:h-6" viewBox="0 0 26 26" />,
     title: "Umrah",
     value: "umrah",
-    component: <UmrahTabpane className="rounded-tl-none min-h-[174px]" />,
+    component: (
+      <UmrahTabpane className="rounded-t-none sm:rounded-tr-md min-h-[174px]" />
+    ),
   },
   {
     id: 3,
@@ -40,7 +44,9 @@ const data = [
     ),
     title: "Visa",
     value: "visa",
-    component: <VisaTabpane className="rounded-tl-none min-h-[174px]" />,
+    component: (
+      <VisaTabpane className="rounded-t-none sm:rounded-tr-md min-h-[174px]" />
+    ),
   },
   {
     id: 4,
@@ -52,18 +58,23 @@ const data = [
     ),
     title: "Customs",
     value: "customs",
-    component: <CustomsTabpane className="rounded-tl-none min-h-[174px]" />,
+    component: (
+      <CustomsTabpane className="rounded-t-none sm:rounded-tr-md min-h-[174px]" />
+    ),
   },
 ];
 
 const Filters = () => {
   return (
-    <div className="relative -mt-0.5">
-      <Image
-        src={BgCurve}
-        alt="Bg curve"
-        className="absolute top-0 left-0 w-full -z-10 object-cover object-center"
-      />
+    <div className="relative -mt-[1px]">
+      <div className="absolute top-0 left-0 w-full -z-10 h-3/4 lg:h-14 bg-[#FCE6E6] lg:bg-p-300/60">
+        <div className="absolute top-full left-0 w-full -mt-[1px] text-[#FCE6E6] lg:text-p-300/60">
+          <BgCurve
+            viewBox="0 0 1439 198"
+            className="w-full h-auto object-cover object-center"
+          />
+        </div>
+      </div>
       <Container>
         <Tabs data={data} defaultValue={data[1].value} />
       </Container>
