a8d1957c1e0657e3ebdf1098a86f46fcf4344763
haquesomrat <ihsomrat.official@gmail.com>
2024-05-29 22:14:45 +0600

    change travellers filter box, scroll area font weight, visa box padding and data texts

--- components/global/VisaTabpane.jsx
+++ components/global/VisaTabpane.jsx
@@ -20,49 +20,49 @@ import { useRouter } from "next/navigation";
 const destinations = [
   {
     id: 1,
-    name: "United states",
-    code: "US",
+    name: "Saudi Arabia",
+    code: "SA",
   },
-  {
-    id: 2,
-    name: "Australia",
-    code: "AU",
-  },
-  {
-    id: 3,
-    name: "Canada",
-    code: "CA",
-  },
-  {
-    id: 4,
-    name: "Bangladesh",
-    code: "BD",
-  },
-  {
-    id: 5,
-    name: "India",
-    code: "IN",
-  },
-  {
-    id: 6,
-    name: "Singapore",
-    code: "SG",
-  },
-  {
-    id: 7,
-    name: "Thailand",
-    code: "TH",
-  },
-  {
-    id: 8,
-    name: "Sri Lanka",
-    code: "LK",
-  },
-  {
-    id: 9,
-    name: "United Kingdom",
-    code: "UK",
-  },
+  // {
+  //   id: 2,
+  //   name: "Australia",
+  //   code: "AU",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Canada",
+  //   code: "CA",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Bangladesh",
+  //   code: "BD",
+  // },
+  // {
+  //   id: 5,
+  //   name: "India",
+  //   code: "IN",
+  // },
+  // {
+  //   id: 6,
+  //   name: "Singapore",
+  //   code: "SG",
+  // },
+  // {
+  //   id: 7,
+  //   name: "Thailand",
+  //   code: "TH",
+  // },
+  // {
+  //   id: 8,
+  //   name: "Sri Lanka",
+  //   code: "LK",
+  // },
+  // {
+  //   id: 9,
+  //   name: "United Kingdom",
+  //   code: "UK",
+  // },
 ];
 const nationalities = [
   "United states",
@@ -75,7 +75,7 @@ const nationalities = [
   "Sri Lanka",
   "United Kingdom",
 ];
-const types = ["Tourist Visa", "Medical Visa", "Business Visa"];
+const types = ["Umrah Visa"];
 const travellers = [
   {
     id: 1,
@@ -101,9 +101,9 @@ const VisaTabpane = ({ icon, disabled, className }) => {
   const router = useRouter();
   const [isDisabled, setIsDisabled] = useState(disabled);
   const [open, setOpen] = useState(0);
-  const [destination, setDestination] = useState("Bangladesh");
+  const [destination, setDestination] = useState("Saudi Arabia");
   const [nationality, setNationality] = useState("Bangladesh");
-  const [type, setType] = useState("Tourist Type");
+  const [type, setType] = useState("Umrah Visa");
   const [traveller, setTraveller] = useState(1);
   const [travellerCounts, setTravellerCounts] = useState(travellers);
   const [errors, setErrors] = useState(null);
@@ -184,7 +184,7 @@ const VisaTabpane = ({ icon, disabled, className }) => {
   return (
     <div
       className={cn(
-        "bg-white flex flex-col lg:flex-row gap-x-4 gap-y-5 lg:gap-x-5 px-2 py-4 xs:px-4 xs:py-6 lg:px-6 sm:py-8 md:py-10 lg:py-12 rounded-md shadow-[0_3px_12px_0_rgba(0,0,0,0.03)]",
+        "bg-white flex flex-col lg:flex-row gap-x-4 gap-y-3 lg:gap-5 px-4 py-5 xs:px-4 xs:py-6 lg:px-6 sm:py-8 md:py-10 lg:py-12 rounded-md shadow-[0_2px_8px_0_rgba(0,0,0,0.07)] lg:shadow-[0_3px_12px_0_rgba(0,0,0,0.03)]",
         className
       )}
     >
@@ -205,14 +205,12 @@ const VisaTabpane = ({ icon, disabled, className }) => {
               }
             )}
           >
-            <span className="text-sm text-t-600 font-normal">Destination</span>
+            <span className="text-xs lg:text-sm text-t-700 lg:text-t-600 font-normal">
+              Destination
+            </span>
             <span className="text-sm lg:text-base flex items-center justify-between gap-x-4 text-t-800 lg:text-t-700 capitalize">
               {destination ? destination : "Select Destination"}
-              <ArrowIcon
-                className={cn("duration-300", {
-                  "transform rotate-180": open === 1,
-                })}
-              />
+              <ArrowIcon className="hidden lg:inline-block" />
             </span>
           </Button>
         </DropdownMenuTrigger>
@@ -226,14 +224,19 @@ const VisaTabpane = ({ icon, disabled, className }) => {
                 key={item.id}
                 onClick={() => setDestination(item.name)}
                 className={cn(
-                  "flex justify-between items-center px-5 py-2.5 text-base text-t-800 font-medium hover:bg-p-900/5 focus:bg-p-900/5 hover:text-p-900 focus:text-p-900 rounded-none cursor-pointer group",
+                  "flex justify-between items-center px-5 py-2.5 text-base text-t-800 hover:bg-p-900/5 focus:bg-p-900/5 hover:text-p-900 focus:text-p-900 rounded-none cursor-pointer group",
                   {
-                    "bg-p-900 text-white": destination === item,
+                    "bg-p-900 text-white": destination === item.name,
                   }
                 )}
               >
                 <span>{item.name}</span>
-                <span className="text-t-600 group-hover:text-p-900 group-focus:text-p-900">
+                <span
+                  className={cn(
+                    "text-t-600 group-hover:text-p-900 group-focus:text-p-900",
+                    { "text-white": destination === item.name }
+                  )}
+                >
                   {item.code}
                 </span>
               </DropdownMenuItem>
@@ -258,14 +261,12 @@ const VisaTabpane = ({ icon, disabled, className }) => {
               }
             )}
           >
-            <span className="text-sm text-t-600 font-normal">Nationality</span>
+            <span className="text-xs lg:text-sm text-t-700 lg:text-t-600 font-normal">
+              Nationality
+            </span>
             <span className="text-sm lg:text-base flex items-center justify-between gap-x-4 text-t-800 lg:text-t-700 capitalize">
               {nationality ? nationality : "Select Nationality"}
-              <ArrowIcon
-                className={cn("duration-300", {
-                  "transform rotate-180": open === 2,
-                })}
-              />
+              <ArrowIcon className="hidden lg:inline-block" />
             </span>
           </Button>
         </DropdownMenuTrigger>
@@ -278,7 +279,7 @@ const VisaTabpane = ({ icon, disabled, className }) => {
               <DropdownMenuItem
                 key={item}
                 className={cn(
-                  "px-5 py-2.5 text-base text-t-700 font-medium hover:bg-p-900/5 focus:bg-p-900/5 hover:text-p-900 focus:text-p-900 rounded-none cursor-pointer",
+                  "px-5 py-2.5 text-base text-t-700 hover:bg-p-900/5 focus:bg-p-900/5 hover:text-p-900 focus:text-p-900 rounded-none cursor-pointer",
                   {
                     "bg-p-900 text-white": nationality === item,
                   }
@@ -308,14 +309,12 @@ const VisaTabpane = ({ icon, disabled, className }) => {
               }
             )}
           >
-            <span className="text-sm text-t-600 font-normal">Visa Type</span>
+            <span className="text-xs lg:text-sm text-t-700 lg:text-t-600 font-normal">
+              Visa Type
+            </span>
             <span className="text-sm lg:text-base flex items-center justify-between gap-x-4 text-t-800 lg:text-t-700 capitalize">
               {type ? type : "Select Type"}
-              <ArrowIcon
-                className={cn("duration-300", {
-                  "transform rotate-180": open === 3,
-                })}
-              />
+              <ArrowIcon className="hidden lg:inline-block" />
             </span>
           </Button>
         </DropdownMenuTrigger>
@@ -329,7 +328,7 @@ const VisaTabpane = ({ icon, disabled, className }) => {
                 key={item}
                 onSelect={() => setType(item)}
                 className={cn(
-                  "px-5 py-2.5 text-base text-t-700 font-medium hover:bg-p-900/5 focus:bg-p-900/5 hover:text-p-900 focus:text-p-900 rounded-none cursor-pointer",
+                  "px-5 py-2.5 text-base text-t-700 hover:bg-p-900/5 focus:bg-p-900/5 hover:text-p-900 focus:text-p-900 rounded-none cursor-pointer",
                   {
                     "bg-p-900 text-white": type === item,
                   }
@@ -344,8 +343,8 @@ const VisaTabpane = ({ icon, disabled, className }) => {
       <DropdownMenu
         open={open === 4}
         onOpenChange={(state) => {
-          setErrors((prevState) => ({ ...prevState, travellerCounts: null }));
-          handleDropdown(state ? 4 : 0);
+          setErrors((prevState) => ({ ...prevState, travellers: null }));
+          handleDropdown(state ? 4 : -1);
         }}
       >
         <DropdownMenuTrigger asChild className="flex-1" disabled={isDisabled}>
@@ -354,48 +353,54 @@ const VisaTabpane = ({ icon, disabled, className }) => {
             className={cn(
               "flex-col items-stretch gap-y-1 text-left focus-visible:ring-transparent p-3.5 border-t-400/50 lg:border-border",
               {
-                "border-p-900 text-p-900": errors?.travellerCounts,
+                "border-p-900 text-p-900": errors?.travellers,
               }
             )}
           >
-            <span className="text-sm text-t-600 font-normal">Traveler</span>
+            <span className="text-xs lg:text-sm text-t-700 lg:text-t-600 font-normal">
+              Travellers
+            </span>
             <span className="text-sm lg:text-base flex items-center justify-between gap-x-4 text-t-800 lg:text-t-700 capitalize">
-              {traveller
-                ? `${traveller} Traveller${traveller > 1 ? "s" : ""}`
+              {travellers.some((item) => item.count > 0)
+                ? `${travellers.reduce(
+                    (acc, item) => acc + item.count,
+                    0
+                  )} Traveller${
+                    travellers.reduce((acc, item) => acc + item.count, 0) > 1
+                      ? "s"
+                      : ""
+                  }`
                 : "Select"}
-              <ArrowIcon
-                className={cn("duration-300", {
-                  "transform rotate-180": open === 4,
-                })}
-              />
+              <ArrowIcon className="hidden lg:inline-block" />
             </span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent
           align="start"
-          className="px-0 py-2 border-transparent min-w-[17rem]"
+          className="px-0 py-2 border-transparent min-w-[308px]"
         >
-          <ScrollArea className="max-h-64">
-            {travellerCounts.map((item) => (
-              <Fragment key={item.id}>
-                <Counter
-                  title={item.title}
-                  description={item.description}
-                  count={item.count}
-                  onIncrement={() => handleCounterIncrement(item.id)}
-                  onDecrement={() => handleCounterDecrement(item.id)}
-                />
-                <DropdownMenuSeparator className="bg-border/50" />
-              </Fragment>
-            ))}
-            <Button
-              size="sm"
-              onClick={() => handleDropdown(0)}
-              className="px-8 py-2 mx-4 my-2"
-            >
-              Done
-            </Button>
-          </ScrollArea>
+          <div className="text-primary font-semibold text-lg px-4 pt-1 pb-5 mb-1">
+            Travelers
+          </div>
+          {travellers.map((item) => (
+            <Fragment key={item.id}>
+              <Counter
+                title={item.title}
+                description={item.description}
+                count={item.count}
+                onIncrement={() => handleCounterIncrement(item.id)}
+                onDecrement={() => handleCounterDecrement(item.id)}
+              />
+              <DropdownMenuSeparator className="bg-border/25 h-[1px]" />
+            </Fragment>
+          ))}
+          <Button
+            size="sm"
+            onClick={() => handleDropdown(0)}
+            className="px-8 py-2 mx-4 my-4 text-sm lg:text-base"
+          >
+            Done
+          </Button>
         </DropdownMenuContent>
       </DropdownMenu>
       <Button
