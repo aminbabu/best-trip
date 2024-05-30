9d3abc570477d75d67407b073de00b83ad3297a9
haquesomrat <ihsomrat.official@gmail.com>
2024-05-31 00:16:24 +0600

    change footer copyright font size 18px to 16px

--- components/layouts/Copyright.jsx
+++ components/layouts/Copyright.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 const Copyright = () => {
   return (
     <div className="pb-10 lg:pb-16">
-      <p className="text-t-600 text-xs xs:text-sm sm:text-base lg:text-lg text-center mb-0">
+      <p className="text-t-600 text-xs xs:text-sm sm:text-base text-center mb-0">
         Best Trip Ltd. | © {moment().format("YYYY")} All Rights Reserved
       </p>
     </div>
