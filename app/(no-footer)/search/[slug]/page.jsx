1cfeda0a174eedd0b35de0324283945fafc853aa
haquesomrat <ihsomrat.official@gmail.com>
2024-06-01 01:41:41 +0600

    filter tab size revert and card count upgraded to 8

--- app/(no-footer)/search/[slug]/page.jsx
+++ app/(no-footer)/search/[slug]/page.jsx
@@ -4,12 +4,7 @@ import BgCurve from "@/public/images/search/curve.svg?url";
 import Filters from "@/components/serach/Filters";
 import FilterResult from "@/components/serach/FilterResult";
 import data from "@/data/umrah-result.json";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { ArrowIcon, PencilIcon, PencliLineIcon } from "@/components/icons/svgr";
+import { ArrowIcon, PencliLineIcon } from "@/components/icons/svgr";
 import { cn } from "@/lib/utils";
 import UmrahTabpane from "@/components/global/UmrahTabpane";
 import { Button } from "@/components/ui/button";
@@ -52,7 +47,7 @@ const SearchPage = async ({ params }) => {
         <Image
           src={BgCurve}
           alt="Bg curve"
-          className="absolute top-0 left-0 w-full 2xl:-mt-24 z-10 object-cover object-center"
+          className="absolute top-0 left-0 w-full -mt-6 2xl:-mt-24 z-10 object-cover object-center hidden lg:block"
         />
 
         <Container className="space-y-6 lg:space-y-8 relative z-20">
@@ -66,7 +61,7 @@ const SearchPage = async ({ params }) => {
           <FilterResult slug={slug} data={data} />
           <div className="relative flex py-4 items-center my-6">
             <div className="flex-grow border-t border-gray-200"></div>
-            <span className="flex-shrink mx-4 text-t-600">End Of Results</span>
+            <span className="flex-shrink mx-4 text-t-600">End of Results</span>
             <div className="flex-grow border-t border-gray-200"></div>
           </div>
         </Container>
