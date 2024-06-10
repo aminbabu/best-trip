54c9c9e376fed1e2a79054b664b426ff7a2aea08
haquesomrat <ihsomrat.official@gmail.com>
2024-06-10 15:28:07 +0600

    change the icon size of navbar profile dropdown

--- data/navbar.js
+++ data/navbar.js
@@ -9,31 +9,31 @@ import {
 export let profileDropDownItems = [
   {
     id: 1,
-    icon: <UserProfileIcon viewBox="0 0 16 18" className="size-5" />,
+    icon: <UserProfileIcon viewBox="0 0 16 18" className="w-5 h-5" />,
     name: "My Profile",
     path: "/profile",
   },
   {
     id: 2,
-    icon: <CalenderTickIcon viewBox="0 0 22 22" className="size-5" />,
+    icon: <CalenderTickIcon viewBox="0 0 22 22" className="w-5 h-5" />,
     name: "My Bookings",
     path: "/bookings",
   },
   {
     id: 3,
-    icon: <CreditCardIcon viewBox="0 0 24 24" className="size-5" />,
+    icon: <CreditCardIcon viewBox="0 0 24 24" className="w-5 h-5" />,
     name: "Payment History",
     path: "/payment-history",
   },
   {
     id: 4,
-    icon: <LockCircleIcon viewBox="0 0 24 24" className="size-5" />,
+    icon: <LockCircleIcon viewBox="0 0 24 24" className="w-5 h-5" />,
     name: "Change Password",
     path: "/change-password",
   },
   {
     id: 5,
-    icon: <SignOutIcon viewBox="0 0 20 21" className="size-5" />,
+    icon: <SignOutIcon viewBox="0 0 20 21" className="w-5 h-5" />,
     name: "Sign Out",
     path: "/signout",
   },
