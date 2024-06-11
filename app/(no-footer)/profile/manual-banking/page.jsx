192cb8989466503d6ae46f3e3a2dc6bd0d3e485e
haquesomrat <ihsomrat.official@gmail.com>
2024-06-11 21:35:08 +0600

    change the payment upload page structure style

--- app/(no-footer)/profile/manual-banking/page.jsx
+++ app/(no-footer)/profile/manual-banking/page.jsx
@@ -85,173 +85,173 @@ const ManualBankingPage = () => {
     <div className="mx-auto">
       <Card className="border-transparent mb-8 shadow-[0px_3px_4px_0px_rgba(0, 0, 0, 0.03)] border border-[#f1f1f4] p-6 lg:p-10">
         <CardHeader className="p-0">
-          <CardTitle className="text-lg lg:text-2xl text-t-800 text-center pb-5 lg:pb-8">
+          <CardTitle className="text-lg lg:text-xl font-medium text-t-800 text-left pb-5 lg:pb-8">
             Payment Upload
           </CardTitle>
         </CardHeader>
         <CardContent className="pb-0 p-0">
           <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-[26px]"
-            >
-              <FormField
-                control={form.control}
-                name="amount"
-                render={({ field }) => (
-                  <FormItem className="space-y-3">
-                    <FormLabel className="text-t-800 text-sm lg:text-base">
-                      Amount<span className="text-primary"> *</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="h-[3.25rem] text-base p-4 text-t-700 placeholder:text-t-600 placeholder:text-sm placeholder:lg:text-base border border-[#f5f5f5] focus-visible:ring-0 focus-visible:ring-offset-0"
-                        placeholder="Enter amount"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage className="font-normal" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="method"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-t-800">
-                      Payment method<span className="text-primary"> *</span>
-                    </FormLabel>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="">
+              <div className="grid grid-cols-2 gap-x-6 gap-y-[26px] mb-[26px]">
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
+                    <FormItem className="space-y-3 col-span-2 lg:col-span-1">
+                      <FormLabel className="text-t-800 text-sm lg:text-base">
+                        Amount<span className="text-primary"> *</span>
+                      </FormLabel>
                       <FormControl>
-                        <SelectTrigger className="h-[3.25rem] text-base p-4 text-t-700 placeholder:text-t-600 placeholder:text-sm placeholder:lg:text-base border border-[#f5f5f5] focus-visible:ring-0 focus-visible:ring-offset-0">
-                          <SelectValue placeholder="Select payment method" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="bank transfer">
-                          Bank Transfer
-                        </SelectItem>
-                        <SelectItem value="bank deposit">
-                          Bank Deposit
-                        </SelectItem>
-                        <SelectItem value="cheque">Chqeque</SelectItem>
-                        <SelectItem value="cash">Cash</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage className="font-normal" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="account"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-t-800">
-                      Deposited A/C<span className="text-primary"> *</span>
-                    </FormLabel>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="h-[3.25rem] text-base p-4 text-t-700 placeholder:text-t-600 placeholder:text-sm placeholder:lg:text-base border border-[#f5f5f5] focus-visible:ring-0 focus-visible:ring-offset-0">
-                          <SelectValue placeholder="Select an account" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="1">
-                          Islami Bank Ltd./ Best Travels Ltd./
-                          13876342617476218/Uttara Branch .
-                        </SelectItem>
-                        <SelectItem value="2">
-                          Pubali Bank Ltd./ Best Travels Ltd./
-                          13876342617476218/Uttara Branch .
-                        </SelectItem>
-                        <SelectItem value="3">
-                          AB Bank Ltd./ Best Travels Ltd./
-                          13876342617476218/Uttara Branch .
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage className="font-normal" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="date"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel className="text-t-800">
-                      Deposited Date<span className="text-primary"> *</span>
-                    </FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant={"outline"}
-                            className="justify-start font-normal h-[3.25rem] text-base p-4 text-t-600 
-                                border-[#f5f5f5] placeholder:text-t-300 disabled:bg-primary-foreground disabled:text-t-600 disabled:border-primary-foreground disabled:opacity-100"
-                          >
-                            {field.value ? (
-                              moment(field.value).format("DD-MMMM-YYYY")
-                            ) : (
-                              <span>Select date</span>
-                            )}
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                          mode="single"
-                          // defaultMonth={moment(field.value).toDate()}
-                          captionLayout="dropdown-buttons"
-                          // selected={moment(field.value).toDate()}
-                          onSelect={field.onChange}
-                          disabled={(date) =>
-                            date > new Date() || date < new Date("1900-01-01")
-                          }
-                          fromYear={1900}
-                          toYear={moment().year()}
-                          initialFocus
+                        <Input
+                          className="h-[3.25rem] text-base p-4 text-t-700 placeholder:text-t-600 placeholder:text-sm placeholder:lg:text-base border border-[#f5f5f5] focus-visible:ring-0 focus-visible:ring-offset-0"
+                          placeholder="Enter amount"
+                          {...field}
                         />
-                      </PopoverContent>
-                    </Popover>
-                    <FormMessage className="font-normal" />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="payment_prove"
-                render={({ field }) => (
-                  <FormItem className="space-y-3">
-                    <FormLabel className="text-t-800">
-                      Attachment<span className="text-primary"> *</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="file"
-                        className="p-0 file:h-[3.25rem] file:mr-4 file:text-sm text-t-700
+                      </FormControl>
+                      <FormMessage className="font-normal" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="method"
+                  render={({ field }) => (
+                    <FormItem className="col-span-2 lg:col-span-1">
+                      <FormLabel className="text-t-800">
+                        Payment method<span className="text-primary"> *</span>
+                      </FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="h-[3.25rem] text-base p-4 text-t-700 placeholder:text-t-600 placeholder:text-sm placeholder:lg:text-base border border-[#f5f5f5] focus-visible:ring-0 focus-visible:ring-offset-0">
+                            <SelectValue placeholder="Select payment method" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="bank transfer">
+                            Bank Transfer
+                          </SelectItem>
+                          <SelectItem value="bank deposit">
+                            Bank Deposit
+                          </SelectItem>
+                          <SelectItem value="cheque">Chqeque</SelectItem>
+                          <SelectItem value="cash">Cash</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage className="font-normal" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="account"
+                  render={({ field }) => (
+                    <FormItem className="col-span-2">
+                      <FormLabel className="text-t-800">
+                        Deposited A/C<span className="text-primary"> *</span>
+                      </FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="h-[3.25rem] text-base p-4 text-t-700 placeholder:text-t-600 placeholder:text-sm placeholder:lg:text-base border border-[#f5f5f5] focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-0">
+                            <SelectValue placeholder="Select an account" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="1">
+                            Islami Bank Ltd./ Best Travels Ltd./
+                            13876342617476218/Uttara Branch .
+                          </SelectItem>
+                          <SelectItem value="2">
+                            Pubali Bank Ltd./ Best Travels Ltd./
+                            13876342617476218/Uttara Branch .
+                          </SelectItem>
+                          <SelectItem value="3">
+                            AB Bank Ltd./ Best Travels Ltd./
+                            13876342617476218/Uttara Branch .
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage className="font-normal" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <FormItem className="col-span-2 lg:col-span-1 flex flex-col gap-3">
+                      <FormLabel className="text-t-800">
+                        Deposited Date<span className="text-primary"> *</span>
+                      </FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className="justify-start font-normal h-[3.25rem] text-base p-4 text-t-600 
+                                border-[#f5f5f5] placeholder:text-t-300 disabled:bg-primary-foreground disabled:text-t-600 disabled:border-primary-foreground disabled:opacity-100"
+                            >
+                              {field.value ? (
+                                moment(field.value).format("DD-MMMM-YYYY")
+                              ) : (
+                                <span>Select date</span>
+                              )}
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            // defaultMonth={moment(field.value).toDate()}
+                            captionLayout="dropdown-buttons"
+                            // selected={moment(field.value).toDate()}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date > new Date() || date < new Date("1900-01-01")
+                            }
+                            fromYear={1900}
+                            toYear={moment().year()}
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage className="font-normal" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="payment_prove"
+                  render={({ field }) => (
+                    <FormItem className="space-y-3 col-span-2 lg:col-span-1">
+                      <FormLabel className="text-t-800">
+                        Attachment<span className="text-primary"> *</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="file"
+                          className="p-0 file:h-[3.25rem] file:mr-4 file:text-sm text-t-700
                             border-[#f5f5f5] placeholder:text-t-300 file:p-4 file:rounded-l-md file:border-0 file:bg-p-300 file:text-p-900"
-                        accept=".png,.jpg,.jpeg"
-                        {...field}
-                        onChange={(event) => {
-                          const { file, displayUrl } = getImageData(event);
-                          field.onChange(event?.target?.value);
-                          setFile(file);
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage className="font-normal" />
-                  </FormItem>
-                )}
-              />
+                          accept=".png,.jpg,.jpeg"
+                          {...field}
+                          onChange={(event) => {
+                            const { file, displayUrl } = getImageData(event);
+                            field.onChange(event?.target?.value);
+                            setFile(file);
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage className="font-normal" />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
               {/* <FormField
                     control={form.control}
                     name="remarks"
@@ -305,7 +305,7 @@ const ManualBankingPage = () => {
               <div className="grid">
                 <Button type="submit" disabled={loading}>
                   {loading && <Loader className="w-6 h-6 animate-spin" />}
-                  Sign Request
+                  Submit
                 </Button>
               </div>
             </form>
