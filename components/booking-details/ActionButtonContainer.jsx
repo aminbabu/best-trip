514cd8403379f8e79ad4b08950721a1ea552c842
haquesomrat <ihsomrat.official@gmail.com>
2024-06-30 11:44:49 +0600

    add booking history action button dialog

--- components/booking-details/ActionButtonContainer.jsx
+++ components/booking-details/ActionButtonContainer.jsx
@@ -24,6 +24,7 @@ import Link from "next/link";
 import DepositForm from "../payment-method/DepositForm";
 import { InfoIcon } from "lucide-react";
 import Swal from "sweetalert2";
+import { Card, CardContent } from "../ui/card";
 
 const ActionButtonContainer = () => {
   const handleCancelBooking = () => {
@@ -116,71 +117,9 @@ const ActionButtonContainer = () => {
         </DialogContent>
       </Dialog>
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="bg-white text-base font-normal text-p-900 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
-            Account Balance Low
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="name"
-                defaultValue="Pedro Duarte"
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Username
-              </Label>
-              <Input
-                id="username"
-                defaultValue="@peduarte"
-                className="col-span-3"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-
-      {/* <Dialog>
-        <DialogTrigger asChild>
-          <Button className="bg-white text-base font-normal text-t-700 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
-            <FlightTokenIcon /> Cancel Booking
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[416px]">
-          <DialogHeader className="items-center">
-            <InfoIcon className="w-20 h-20 text-[#f78212] mb-5" />
-            <DialogDescription>
-              Are you sure you would like to cancel it?
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter className="sm:justify-center mt-5 mb-3">
-            <Button className="px-4 py-2 text-sm font-normal" type="submit">
-              Yes, cancel it!
-            </Button>{" "}
-            <Button className="px-4 py-2 text-sm font-normal bg-slate-50 text-t-700 hover:bg-slate-100">
-              No, return
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog> */}
+      <Button className="bg-white text-base font-normal text-p-900 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
+        Account Balance Low
+      </Button>
 
       <Button
         onClick={handleCancelBooking}
@@ -201,6 +140,7 @@ const ActionButtonContainer = () => {
       <Button className="bg-white text-base font-normal text-t-700 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
         <FlightTokenIcon /> Ticket Number Update
       </Button>
+
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1" className="border-none">
           <AccordionTrigger className="bg-white text-base font-normal text-t-700 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] hover:no-underline justify-start gap-x-4">
@@ -233,9 +173,70 @@ const ActionButtonContainer = () => {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
-      <Button className="bg-white text-base font-normal text-t-700 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
-        <FlightTokenIcon /> Booking History
-      </Button>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="bg-white text-base font-normal text-t-700 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
+            <FlightTokenIcon /> Booking History
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[800px] p-0">
+          <DialogHeader className="bg-p-300 p-5 rounded">
+            <DialogTitle className="text-xl text-t-800">
+              Booking History
+            </DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 p-5">
+            <div className="w-full overflow-auto drop-shadow-md">
+              <div className="grid grid-rows-3 min-w-max lg:min-w-fit text-base text-center overflow-hidden text-t-800">
+                <ul className="grid grid-cols-7 bg-[#FFEFEF]">
+                  <li className="flex items-center col-span-2 justify-center px-2.5 py-1.5">
+                    Created On
+                  </li>
+                  <li className="flex items-center col-span-5 justify-center px-2.5 py-1.5 border-l border-white">
+                    Description
+                  </li>
+                </ul>
+                <ul className="grid grid-cols-7 bg-white">
+                  <li className="flex items-center col-span-2 justify-center px-2.5 py-1.5 border border-[#3D3D3D0D]">
+                    15/07/2023 17:32:55
+                  </li>
+                  <li className="text-left col-span-5 px-2.5 py-1.5 border border-[#3D3D3D0D]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </li>
+                </ul>
+                <ul className="grid grid-cols-7 bg-white">
+                  <li className="flex items-center col-span-2 justify-center px-2.5 py-1.5 border border-[#3D3D3D0D]">
+                    15/07/2023 17:32:55
+                  </li>
+                  <li className="col-span-5 px-2.5 py-1.5 border border-[#3D3D3D0D] text-left">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Ut, odit!
+                  </li>
+                </ul>
+                <ul className="grid grid-cols-7 bg-white">
+                  <li className="flex items-center col-span-2 justify-center px-2.5 py-1.5 border border-[#3D3D3D0D]">
+                    15/07/2023 17:32:55
+                  </li>
+                  <li className="text-left col-span-5 px-2.5 py-1.5 border border-[#3D3D3D0D]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                  </li>
+                </ul>
+                <ul className="grid grid-cols-7 bg-white">
+                  <li className="flex items-center col-span-2 justify-center px-2.5 py-1.5 border border-[#3D3D3D0D]">
+                    15/07/2023 17:32:55
+                  </li>
+                  <li className="col-span-5 px-2.5 py-1.5 border text-left border-[#3D3D3D0D]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+                    provident id quia distinctio.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+
       <Button className="bg-white text-base font-normal text-t-700 rounded shadow-sm px-3.5 py-5 hover:bg-[#fefefe] justify-start">
         <FlightTokenIcon /> Extend Time Limit
       </Button>
