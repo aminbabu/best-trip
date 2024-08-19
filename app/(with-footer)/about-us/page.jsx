56c2e746c334a5f62f426783b757f79462b07443
Amin Babu <amin.babu.bd@gmail.com>
2024-08-19 20:19:16 +0600

    Integrate about us api

--- app/(with-footer)/about-us/page.jsx
+++ app/(with-footer)/about-us/page.jsx
@@ -1,105 +1,27 @@
+import { getPrivacySiteSettings } from "@/actions/site-settings/get-site-settings";
+import InnerHTML from "@/components/global/inner-html";
 import Container from "@/components/layouts/Container";
 
-const RefundPolicyPage = () => {
+const AboutUsPage = async () => {
+  let aboutUs = [];
+
+  try {
+    const policies = await getPrivacySiteSettings();
+
+    aboutUs = policies[0]?.["about-us"];
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <main>
       <section className="py-12 lg:py-16">
-        <Container className="[&_p]:text-t-600 [&_p]:mb-8">
-          <h2 className="text-lg text-t-800 font-semibold mb-3.5">About Us</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-            unde officiis, voluptatibus facilis distinctio vel corporis vero a,
-            nesciunt cum accusamus. Placeat labore debitis officia! Ab laborum
-            magni nihil accusantium!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-            necessitatibus dolor, possimus dolores non deserunt! Autem dicta
-            natus velit ipsum quae dolorem! Earum assumenda laudantium rerum
-            culpa maiores beatae odit autem amet odio asperiores minima, itaque
-            cumque voluptates ea veniam neque at dolore dicta rem. Suscipit
-            officiis enim quidem eveniet, reiciendis non aperiam quaerat vel sit
-            nostrum exercitationem. Molestias itaque debitis tenetur architecto
-            perspiciatis accusantium perferendis est quia beatae, temporibus
-            molestiae voluptates, unde fugiat. Nulla debitis praesentium
-            deserunt sequi quia officiis quos expedita dolorem? Expedita nisi
-            vel quo iusto ipsam a sequi dolorem, iste velit accusamus hic
-            aperiam. Molestias, aspernatur.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia
-            aspernatur facilis eaque eius eligendi. In ipsam aliquid et,
-            doloribus explicabo ea voluptas, tempora aperiam veritatis odit
-            pariatur reprehenderit a error consectetur recusandae illum
-            molestiae velit exercitationem ex, placeat minus. Voluptate
-            dignissimos dicta fuga fugit nisi necessitatibus aperiam mollitia
-            eaque alias, quod voluptatem. Quam tenetur soluta dolores, voluptate
-            quisquam error commodi.
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repudiandae corrupti quia odio. Dicta quibusdam officia veritatis
-            quod iusto veniam, soluta beatae? Quas minima hic aut,
-            exercitationem ex, rerum labore enim libero aspernatur harum a
-            voluptatibus asperiores adipisci tempore nulla sit numquam
-            recusandae doloribus consectetur optio aliquam nobis. At nemo non
-            qui nam sint dolorem veniam fugiat molestiae ullam, quos eveniet
-            blanditiis totam facilis assumenda eos, deserunt doloremque
-            exercitationem asperiores iste quam molestias, repudiandae repellat
-            voluptate. Debitis velit deserunt repellendus dolores dicta iure,
-            natus corrupti in, pariatur aliquam atque totam? Impedit.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-            deleniti voluptate provident officiis, unde, consequuntur dolore
-            voluptates repellendus saepe nulla magni maxime totam similique,
-            dignissimos consequatur. Saepe esse quia voluptatum.
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-            delectus consequuntur optio debitis suscipit, quod expedita
-            obcaecati! Minus modi asperiores libero qui ratione voluptates nisi,
-            inventore omnis reiciendis quod consequatur eaque facilis natus
-            vitae fuga molestiae. Consectetur, nostrum quisquam? Facilis aperiam
-            ullam, distinctio eum corporis voluptatem unde a aspernatur eveniet
-            veritatis debitis cupiditate dicta optio autem vel commodi ut
-            dolore, repudiandae nobis quam consectetur nemo expedita perferendis
-            nostrum! Dicta tempore est ducimus, corrupti laudantium sapiente!
-            Qui, quisquam pariatur! At tenetur dolor quos fugiat laborum enim
-            rem cumque minus nobis corrupti itaque, quas esse saepe hic
-            accusantium. A aspernatur officia quo delectus deleniti harum
-            provident culpa corporis vitae consequatur, nemo eligendi.
-            Repudiandae ea odio porro provident accusamus facere dolorem ipsam
-            vitae tempore delectus voluptate, exercitationem est, consequatur
-            assumenda nisi soluta animi vero rerum numquam, asperiores id.
-            Labore deserunt cumque tempore deleniti rem amet, aut aspernatur.
-            Non dignissimos nam cum officia accusamus suscipit impedit enim fuga
-            quia. In numquam qui facilis labore sint. Iste, nobis, architecto
-            aliquid iure doloremque accusamus quidem eaque voluptatum sunt
-            explicabo ex earum pariatur odio rem amet quaerat illo in. Modi
-            fugiat id voluptatem dignissimos dolore. Libero, repellat? Explicabo
-            aut iusto harum vel accusantium quam saepe repellendus facere?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-            quibusdam eos temporibus. Excepturi blanditiis qui alias illum.
-            Eveniet aspernatur tempore quae labore ullam. Hic praesentium ab non
-            saepe molestias, magni provident error ea? Asperiores natus quasi
-            reiciendis, ipsam et dolores temporibus doloribus. Quas velit,
-            expedita molestias adipisci nesciunt atque tempore suscipit quisquam
-            aperiam minus in. Tempore ullam quia veritatis assumenda voluptatem?
-            Sapiente eveniet aliquid adipisci impedit maiores nesciunt quis nam
-            pariatur neque laboriosam, libero inventore laudantium odio eos
-            beatae alias iure fugiat facere cumque eius sed amet, tempore autem
-            consectetur? Dicta ullam vitae eos porro sequi aliquam, fuga vero
-            optio sapiente veritatis nesciunt alias dolore, quis suscipit!
-            Consequatur, velit. Ut deleniti numquam inventore molestias eius
-            explicabo neque vero nobis suscipit.
-          </p>
+        <Container>
+          <InnerHTML dangerouslySetInnerHTML={aboutUs} />
         </Container>
       </section>
     </main>
   );
 };
 
-export default RefundPolicyPage;
+export default AboutUsPage;
