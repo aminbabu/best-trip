2fc610585a61ff45244d236d5b703b82dd7db743
Amin Babu <amin.babu.bd@gmail.com>
2024-08-19 20:33:59 +0600

    Add auth gruard

--- app/(no-footer)/profile/layout.jsx
+++ app/(no-footer)/profile/layout.jsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth";
 import Container from "@/components/layouts/Container";
 import Sidebar from "@/components/profile/Sidebar";
+import { redirect } from "next/navigation";
 
-const ProfileLayout = ({ children }) => {
+const ProfileLayout = async ({ children }) => {
+  const { user } = (await auth()) || {};
+
+  if (!user?._id) {
+    redirect("/sign-in");
+  }
+
   return (
     <section className="py-12 lg:py-16 bg-primary-foreground min-h-[calc(100vh-94px)]">
       <Container className="flex flex-col lg:flex-row lg:items-start gap-8">
