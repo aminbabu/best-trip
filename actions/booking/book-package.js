49df43e103f9207d623f0225f283a5fac0d54c79
haquesomrat <ihsomrat.official@gmail.com>
2024-08-31 12:23:58 +0600

    set cookies to the book package api

--- actions/booking/book-package.js
+++ actions/booking/book-package.js
@@ -1,10 +1,15 @@
 "use server";
 import handleAxiosError from "@/handlers/axios/error";
 import axios from "@/lib/axios";
+import { cookies } from "next/headers";
 
 export const bookPackage = async (data) => {
   try {
     const response = await axios.post("/api/umrah/booking", data);
+    const sevenDays = 7 * 24 * 60 * 60 * 1000;
+    cookies().set("bookingId", response?.data?.data?._id, {
+      expires: Date.now() + sevenDays,
+    });
     return response;
   } catch (error) {
     throw new Error(handleAxiosError(error));
--- components/umrah/BookingForm.jsx
+++ components/umrah/BookingForm.jsx
@@ -1,76 +1,63 @@
 import { Button } from "../ui/button";
 import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
-import { setCookie } from 'cookies-next';
 import { bookPackage } from "@/actions/booking/book-package";
 
 const BookingForm = ({ data, bookingData }) => {
-    const router = useRouter();
+  const router = useRouter();
+  console.log(data);
 
-    const submitBooking = async (event) => {
-        event.preventDefault();
-        
-        if (!data) {
-            Swal.fire({
-                title: "Login Required",
-                text: "You have to log in to book a package!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Ok!",
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    router.push(`/sign-in`);
-                }
-            });
-            return;
+  const submitBooking = async (event) => {
+    event.preventDefault();
+
+    if (!data) {
+      Swal.fire({
+        title: "Login Required",
+        text: "You have to log in to book a package!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Ok!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          router.push(`/sign-in`);
         }
+      });
+      return;
+    }
 
-        try {
-            const response = await bookPackage(bookingData);
-            const bookingId = response.data.data._id;
+    try {
+      const response = await bookPackage(bookingData);
+      const bookingId = response?.data?.data?._id;
+      return console.log(response);
 
-            // Set bookingId in localStorage and cookies
-            if (typeof window !== "undefined") {
+      Swal.fire({
+        title: `Booking Successful! BookingId: ${bookingId}`,
+        text: "Your package booking was successful.",
+        icon: "success",
+        confirmButtonColor: "#3085d6",
+      });
 
-                setCookie("bookingId", bookingId, { expires: 7 }); // Expires in 7 days
-            }
+      router.push(`/umrah/${bookingId}/traveller-details`);
+    } catch (error) {
+      console.error(error);
 
-            Swal.fire({
-                title: `Booking Successful! BookingId: ${bookingId}`,
-                text: "Your package booking was successful.",
-                icon: "success",
-                confirmButtonColor: "#3085d6",
-            });
+      if (error?.response?.data?.id) {
+        const bookingId = error?.response?.data?.id;
 
-            router.push(`/umrah/${bookingId}/traveller-details`);
-        } catch (error) {
-            console.error(error);
+        router.push(`/umrah/${bookingId}/traveller-details`);
+      }
+    }
+  };
 
-            if (error?.response?.data?.id) {
-                const bookingId = error.response.data.id;
-
-                // Set bookingId in localStorage and cookies
-                if (typeof window !== "undefined") {
-                    localStorage.setItem("bookingId", bookingId);
-                    Cookies.set("bookingId", bookingId, { expires: 7 });
-                }
-
-                router.push(`/umrah/${bookingId}/traveller-details`);
-            }
-        }
-    };
-
-    return (
-        <form onSubmit={submitBooking}>
-            <div className="grid">
-                <Button type="submit">
-                    Continue Booking
-                </Button>
-            </div>
-        </form>
-    );
+  return (
+    <form onSubmit={submitBooking}>
+      <div className="grid">
+        <Button type="submit">Continue Booking</Button>
+      </div>
+    </form>
+  );
 };
 
 export default BookingForm;
